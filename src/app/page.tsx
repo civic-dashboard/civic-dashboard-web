aa1274fc8bbe8c36357d65745504266a60846b23
shrouxm <mushrouxm@gmail.com>
2024-11-17 16:22:24 -0800

    feat: clientside agenda item search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { fetchDecisionBody } from '@/api/decisionbody';
+import { fetchItems } from '@/api/item';
 import { EventList } from '@/components/EventList';
 import { ItemList } from '@/components/ItemList';
 
-export default function Home() {
+export default async function Home() {
+  const items = await fetchItems();
+  const decisionBodyIds = new Set(items.map((item) => item.decisionBodyId));
+  const decisionBodies = Object.fromEntries(
+    (
+      await Promise.all([...decisionBodyIds.values()].map(fetchDecisionBody))
+    ).map((body) => [body.decisionBodyId, body])
+  );
   return (
     <div className="flex flex-row bg-white min-w-[700px]">
       <div className="w-1/2 m-4">
         <h1>Upcoming Agenda Items</h1>
-        <ItemList />
+        <ItemList items={items} decisionBodies={decisionBodies} />
       </div>
       <div className="w-1/2 m-4">
         <h1>Public Consultations</h1>
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,18 +1,65 @@
-import { fetchDecisionBody } from '@/api/decisionbody';
-import { fetchItems } from '@/api/item';
-import { ItemCard } from '@/components/ItemCard';
+'use client';
 
-export async function ItemList() {
-  const items = await fetchItems();
-  const decisionBodyIds = new Set(items.map((item) => item.decisionBodyId));
-  const decisionBodies = Object.fromEntries(
-    (
-      await Promise.all([...decisionBodyIds.values()].map(fetchDecisionBody))
-    ).map((body) => [body.decisionBodyId, body])
-  );
+import { DecisionBody } from '@/api/decisionbody';
+import { AgendaItem } from '@/api/item';
+import { ItemCard } from '@/components/ItemCard';
+import FlexSearch from 'flexsearch';
+import { Search } from 'lucide-react';
+import { useMemo, useState } from 'react';
+
+type Props = {
+  items: AgendaItem[];
+  decisionBodies: Record<string, DecisionBody>;
+};
+
+export function ItemList({ items, decisionBodies }: Props) {
+  const [searchText, setSearchText] = useState('');
+
+  const searchIndex = useMemo(() => {
+    const searchIndex = new FlexSearch.Document<AgendaItem, true>({
+      tokenize: 'forward',
+      document: {
+        id: 'id',
+        index: ['agendaItemTitle', 'decisionBodyName', 'agendaItemSummary'],
+        store: true,
+      },
+    });
+
+    for (const item of items) {
+      searchIndex.add(item);
+    }
+
+    return searchIndex;
+  }, [items]);
+
+  const searchResults = useMemo(() => {
+    if (searchText === '') {
+      return items;
+    }
+    const results = searchIndex.search(searchText, undefined, { enrich: true });
+    const filteredResults = [];
+    const existingKeys = new Set<string>();
+    for (const fieldResult of results) {
+      const filteredResult = fieldResult.result
+        .map((r) => r.doc)
+        .filter((d) => !existingKeys.has(d.id));
+      filteredResults.push(...filteredResult);
+      filteredResult.forEach((d) => existingKeys.add(d.id));
+    }
+    return filteredResults;
+  }, [searchIndex, searchText]);
+
   return (
     <div className="flex-col space-y-4 p-4 bg-slate-200">
-      {items?.map((item) => (
+      <div className="flex flex-row space-x-2 items-center">
+        <Search />
+        <input
+          className="p-1 flex-1"
+          onChange={(ev) => setSearchText(ev.target.value)}
+          placeholder="Search agenda items..."
+        />
+      </div>
+      {searchResults.map((item) => (
         <ItemCard
           key={item.id}
           item={item}
