aa15ad05920aaad5b023bdd01758db868f10a47a
shrouxm <mushrouxm@gmail.com>
2024-12-17 13:51:21 -0800

    feat: nav menu ui overhaul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { fetchDecisionBodies } from '@/api/decisionBody';
 import { fetchItems } from '@/api/agendaItem';
-import { PublicConsultationList } from '@/components/PublicConsultationList';
 import { AgendaItemList } from '@/components/AgendaItemList';
 
 export default async function Home() {
@@ -8,15 +7,8 @@ export default async function Home() {
   const decisionBodies = await fetchDecisionBodies();
 
   return (
-    <div className="flex flex-row bg-white min-w-[700px]">
-      <div className="w-1/2 m-4">
-        <h1>Upcoming Agenda Items</h1>
-        <AgendaItemList items={items} decisionBodies={decisionBodies} />
-      </div>
-      <div className="w-1/2 m-4">
-        <h1>Public Consultations</h1>
-        <PublicConsultationList />
-      </div>
+    <div className="flex flex-col items-center">
+      <AgendaItemList items={items} decisionBodies={decisionBodies} />
     </div>
   );
 }
