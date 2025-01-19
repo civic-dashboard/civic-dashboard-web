7632a1bb8217d284415f8cd870612b8bca3415e3
Matt Orchard <morch028@uottawa.ca>
2025-01-19 16:28:59 -0500

    Swap to single env var for DB connection

--- src/lib/psql.ts
+++ src/lib/psql.ts
@@ -1,15 +1,9 @@
 import { Pool } from 'pg';
 
 export const createPool = () => {
-  const config = {
-    host: process.env.PGHOST,
-    port: +process.env.PGPORT!,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-  };
-  for (const [key, value] of Object.entries(config)) {
-    if (!value) throw new Error(`Missing PSQL configuration option "${key}"`);
-  }
-  return new Pool(config);
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString)
+    throw new Error(`No value for "DATABASE_URL" provided`);
+
+  return new Pool({ connectionString });
 };
