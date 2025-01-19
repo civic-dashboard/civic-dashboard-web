750860cfcbf12d81fb1385c28489ba0d182274e7
Matt Orchard <morch028@uottawa.ca>
2025-01-18 21:26:17 -0500

    Include logging in migrations

--- kysely.config.ts
+++ kysely.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'kysely-ctl';
+
+import { Kysely, PostgresDialect } from 'kysely';
+
 // Kysely CLI does not respect import-aliases yet
 // eslint-disable-next-line no-restricted-imports
 import { createPool } from './src/lib/psql';
 
 export default defineConfig({
-  dialect: 'pg',
-  dialectConfig: {
-    pool: createPool(),
-  },
+  kysely: new Kysely({
+    dialect: new PostgresDialect({
+      pool: createPool(),
+    }),
+    log: ['query', 'error'],
+  }),
   migrations: {
     migrationFolder: 'src/migrations',
   },
