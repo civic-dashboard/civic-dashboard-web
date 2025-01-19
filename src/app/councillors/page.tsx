905cd5e3b190eb09b7fe854143f5452ca622bbc5
Matt Orchard <morch028@uottawa.ca>
2025-01-19 16:50:38 -0500

    Add councillor list page content

--- src/app/councillors/page.tsx
+++ src/app/councillors/page.tsx
@@ -1,4 +1,5 @@
 import { db } from '@/lib/kdb';
+import Link from 'next/link';
 
 async function listCouncillors() {
   return await db
@@ -15,10 +16,47 @@ async function listCouncillors() {
       'Contacts.photoUrl',
       'Wards.wardName',
     ])
+    .orderBy(['Contacts.contactName'])
     .execute();
 }
 
 export default async function CouncillorListPage() {
   const councillors = await listCouncillors();
-  return <div>{JSON.stringify(councillors)}</div>;
+  return (
+    <div className="max-w-screen-sm mx-auto mt-3">
+      <h2>Toronto Councillors</h2>
+      <ul>
+        {councillors.map((councillor) => (
+          <li
+            key={councillor.contactSlug}
+            className="border border-slate-200 bg-white even:bg-slate-50 text-slate-950 shadow-sm dark:border-slate-800 dark:bg-slate-950 even:dark:bg-slate-900 dark:text-slate-50 rounded-md mb-2"
+          >
+            <Link
+              href={`/councillors/${councillor.contactSlug}`}
+              className="p-2 flex gap-2 hover:underline"
+              prefetch={false}
+            >
+              <CouncillorContactPhoto photoUrl={councillor.photoUrl} />
+              <div>
+                <h3 className="text-lg">{councillor.contactName}</h3>
+                <p>{councillor.wardName}</p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function CouncillorContactPhoto(props: { photoUrl: string | null }) {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={props.photoUrl ?? ''}
+      className="bg-zinc-300"
+      style={{ height: 52, width: 42 }}
+      alt="" // Omit alt text since this is decorative
+    />
+  );
 }
