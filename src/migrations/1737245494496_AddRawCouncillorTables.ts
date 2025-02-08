0b2dc07ef86f3fcd77e155340aea73db1ecb8090
Matt Orchard <morch028@uottawa.ca>
2025-02-08 14:22:34 -0500

    Port migration for raw tables into raw SQL

--- src/migrations/1737245494496_AddRawCouncillorTables.ts
+++ src/migrations/1737245494496_AddRawCouncillorTables.ts
@@ -1,46 +1,47 @@
 import { sql, type Kysely } from 'kysely';
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  await db.schema
-    .createTable('RawVotes')
-    .addColumn('term', 'text', (col) => col.notNull())
-    .addColumn('committeeName', 'text', (col) => col.notNull())
-    .addColumn('committeeSlug', 'text', (col) => col.notNull())
-    .addColumn('dateTime', 'text', (col) => col.notNull())
-    .addColumn('agendaItemNumber', 'text', (col) => col.notNull())
-    .addColumn('agendaItemTitle', 'text', (col) => col.notNull())
-    .addColumn('motionId', 'text', (col) => col.notNull())
-    .addColumn('motionType', 'text', (col) => col.notNull())
-    .addColumn('vote', 'text', (col) => col.notNull())
-    .addColumn('result', 'text', (col) => col.notNull())
-    .addColumn('voteDescription', 'text', (col) => col.notNull())
-    .addColumn('inputRowNumber', 'bigint', (col) => col.notNull())
-    .addColumn('contactName', 'text', (col) => col.notNull())
-    .addColumn('contactSlug', 'text', (col) => col.notNull())
-    .addColumn('movedBy', 'text')
-    .addColumn('secondedBy', sql`text[]`)
-    .execute();
-
-  await db.schema
-    .createTable('RawContacts')
-    .addColumn('primaryRole', 'text', (col) => col.notNull())
-    .addColumn('email', 'text', (col) => col.notNull())
-    .addColumn('photoUrl', 'text')
-    .addColumn('website', 'text')
-    .addColumn('addressLine1', 'text')
-    .addColumn('addressLine2', 'text')
-    .addColumn('locality', 'text')
-    .addColumn('postalCode', 'text')
-    .addColumn('province', 'text')
-    .addColumn('phone', 'text')
-    .addColumn('fax', 'text')
-    .addColumn('personalWebsite', 'text')
-    .addColumn('term', 'text', (col) => col.notNull())
-    .addColumn('inputRowNumber', 'bigint', (col) => col.notNull())
-    .addColumn('wardId', 'text')
-    .addColumn('wardName', 'text')
-    .addColumn('wardSlug', 'text')
-    .addColumn('contactName', 'text', (col) => col.notNull())
-    .addColumn('contactSlug', 'text', (col) => col.notNull())
-    .execute();
+  await sql`
+    CREATE TABLE "RawContacts" (
+      "primaryRole" TEXT NOT NULL,
+      "email" TEXT NOT NULL,
+      "photoUrl" TEXT NULL,
+      "website" TEXT NULL,
+      "addressLine1" TEXT NULL,
+      "addressLine2" TEXT NULL,
+      "locality" TEXT NULL,
+      "postalCode" TEXT NULL,
+      "province" TEXT NULL,
+      "phone" TEXT NULL,
+      "fax" TEXT NULL,
+      "personalWebsite" TEXT NULL,
+      "term" TEXT NOT NULL,
+      "inputRowNumber" BIGINT NOT NULL,
+      "wardId" TEXT NULL,
+      "wardName" TEXT NULL,
+      "wardSlug" TEXT NULL,
+      "contactName" TEXT NOT NULL,
+      "contactSlug" TEXT NOT NULL
+    );
+  `.execute(db);
+  await sql`
+    CREATE TABLE "RawVotes" (
+      "term" TEXT NOT NULL,
+      "committeeName" TEXT NOT NULL,
+      "committeeSlug" TEXT NOT NULL,
+      "dateTime" TEXT NOT NULL,
+      "agendaItemNumber" TEXT NOT NULL,
+      "agendaItemTitle" TEXT NOT NULL,
+      "motionId" TEXT NOT NULL,
+      "motionType" TEXT NOT NULL,
+      "vote" TEXT NOT NULL,
+      "result" TEXT NOT NULL,
+      "voteDescription" TEXT NOT NULL,
+      "inputRowNumber" BIGINT NOT NULL,
+      "contactName" TEXT NOT NULL,
+      "contactSlug" TEXT NOT NULL,
+      "movedBy" TEXT NULL,
+      "secondedBy" TEXT[] NULL
+    );
+  `.execute(db);
 }
