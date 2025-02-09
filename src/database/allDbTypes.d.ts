6fe41441e3dd4fe0172fa9191bb494ed563e6f11
Matt Orchard <morch028@uottawa.ca>
2025-02-09 15:25:38 -0500

    Shares insert type for raw tables

--- src/database/allDbTypes.d.ts
+++ src/database/allDbTypes.d.ts
@@ -1,6 +1,7 @@
 // This is the one place allowed to import from generated types as here is were we re-export them
 // eslint-disable-next-line no-restricted-imports
 import * as generated from '@/database/generatedDbTypes';
+import { Insertable } from 'kysely';
 
 // Currently keysely codegen does not support materialized views.
 // For now we define their type manually here
@@ -30,3 +31,6 @@ export type DB = generated.DB & {
   Contacts: Contact;
   Wards: Ward;
 };
+
+export type InsertRawContact = Insertable<generated.RawContacts>;
+export type InsertRawVote = Insertable<generated.RawVotes>;
