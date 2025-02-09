f5419e8533f98574a8d3f8a422420a58e7e3cec4
Matt Orchard <morch028@uottawa.ca>
2025-02-09 15:58:01 -0500

    Use pipeline class with shared transaction

--- src/database/pipelines/repopulateRawContactsAndVotes.ts
+++ src/database/pipelines/repopulateRawContactsAndVotes.ts
@@ -2,26 +2,123 @@ import {
   OpenDataClient,
   PackageResource,
 } from '@/backend/open-data/OpenDataClient';
-import { InsertRawContact, InsertRawVote } from '@/database/allDbTypes';
+import { DB, InsertRawContact, InsertRawVote } from '@/database/allDbTypes';
 
 import { db } from '@/database/kyselyDb';
 import { formatContactCsvStream } from '@/database/pipelines/rawContactCsvParser';
 import { formatVoteCsvStream } from '@/database/pipelines/rawVoteCsvParser';
 import { extractTermFromText } from '@/database/pipelines/textParseUtils';
-import { sql } from 'kysely';
-
-const INSERT_BATCH_SIZE = 2_500;
+import { Transaction, sql } from 'kysely';
 
 export async function repopulateRawContactsAndVotes() {
   try {
-    const openDataClient = new OpenDataClient();
-    void repopulateRawContacts;
-    await repopulateRawContacts(openDataClient);
-    await repopulateRawVotes(openDataClient);
-    await refreshMatViews();
+    await db.transaction().execute(async (trx) => {
+      await RepopulateRawContactsAndVotesPipeline.run(trx);
+    });
   } catch (error) {
     console.error(`Error during repopulateRawContactsAndVotes`, error);
     throw error;
+  }
+}
+
+class RepopulateRawContactsAndVotesPipeline {
+  private static BATCH_SIZE = 2_500;
+  private constructor(
+    private readonly trx: Transaction<DB>,
+    private readonly openDataClient: OpenDataClient,
+  ) {}
+
+  public static async run(trx: Transaction<DB>) {
+    const instance = new RepopulateRawContactsAndVotesPipeline(
+      trx,
+      new OpenDataClient(),
+    );
+    return await instance.run();
+  }
+
+  private async run() {
+    await this.repopulateRawContacts();
+    await this.repopulateRawVotes();
+    await this.refreshMatViews();
+  }
+
+  private async refreshMatViews() {
+    await sql`
+      REFRESH MATERIALIZED VIEW "Contacts";
+      REFRESH MATERIALIZED VIEW "Councillors";
+      REFRESH MATERIALIZED VIEW "Wards";
+      REFRESH MATERIALIZED VIEW "Committees";
+      REFRESH MATERIALIZED VIEW "AgendaItems";
+      REFRESH MATERIALIZED VIEW "ProblemAgendaItems";
+      REFRESH MATERIALIZED VIEW "Motions";
+      REFRESH MATERIALIZED VIEW "Votes";
+    `.execute(this.trx);
+  }
+
+  private async repopulateRawContacts() {
+    const contactsPackage = await this.openDataClient.showPackage('contacts');
+    await this.deleteAllRawContacts();
+    for (const resource of contactsPackage.result.resources) {
+      if (!isCompleteCsvResource(resource)) continue;
+      const term = extractTermFromText(resource.name);
+      const requestStream = await this.openDataClient.fetchDataset(
+        resource.url,
+      );
+      await this.bulkInsertRawContacts(
+        formatContactCsvStream(term, requestStream),
+      );
+    }
+  }
+
+  private async repopulateRawVotes() {
+    const votesPackage = await this.openDataClient.showPackage('votes');
+    const [mostRecentVoteResource] = votesPackage.result.resources
+      .filter(isCompleteCsvResource)
+      .map((resource) => ({
+        url: resource.url,
+        term: extractTermFromText(resource.name),
+      }))
+      .sort((a, b) => b.term.localeCompare(a.term));
+    if (!mostRecentVoteResource) {
+      throw new Error(`No suitable voting resources found`, {
+        cause: votesPackage.result.resources,
+      });
+    }
+    const requestStream = await this.openDataClient.fetchDataset(
+      mostRecentVoteResource.url,
+    );
+    await this.deleteAllRawVotes();
+    await this.bulkInsertRawVotes(
+      formatVoteCsvStream(mostRecentVoteResource.term, requestStream),
+    );
+  }
+
+  private async bulkInsertRawContacts(
+    rowStream: AsyncIterable<InsertRawContact>,
+  ) {
+    for await (const batch of asBatches(
+      rowStream,
+      RepopulateRawContactsAndVotesPipeline.BATCH_SIZE,
+    )) {
+      await this.trx.insertInto('RawContacts').values(batch).execute();
+    }
+  }
+
+  private async bulkInsertRawVotes(rowStream: AsyncIterable<InsertRawVote>) {
+    for await (const batch of asBatches(
+      rowStream,
+      RepopulateRawContactsAndVotesPipeline.BATCH_SIZE,
+    )) {
+      await this.trx.insertInto('RawVotes').values(batch).execute();
+    }
+  }
+
+  private async deleteAllRawContacts() {
+    await sql`TRUNCATE "RawContacts";`.execute(this.trx);
+  }
+
+  private async deleteAllRawVotes() {
+    await sql`TRUNCATE "RawVotes";`.execute(this.trx);
   }
 }
 
@@ -29,53 +126,6 @@ const isCompleteCsvResource = (resource: PackageResource) =>
   !resource.is_preview &&
   resource.format.toLocaleLowerCase() === 'csv' &&
   resource.url.endsWith('.csv');
-
-const refreshMatViews = async () => {
-  await sql`
-    REFRESH MATERIALIZED VIEW "Contacts";
-    REFRESH MATERIALIZED VIEW "Councillors";
-    REFRESH MATERIALIZED VIEW "Wards";
-    REFRESH MATERIALIZED VIEW "Committees";
-    REFRESH MATERIALIZED VIEW "AgendaItems";
-    REFRESH MATERIALIZED VIEW "ProblemAgendaItems";
-    REFRESH MATERIALIZED VIEW "Motions";
-    REFRESH MATERIALIZED VIEW "Votes";
-  `.execute(db);
-};
-
-async function repopulateRawVotes(openDataClient: OpenDataClient) {
-  const votesPackage = await openDataClient.showPackage('votes');
-  const [mostRecentVoteResource] = votesPackage.result.resources
-    .filter(isCompleteCsvResource)
-    .map((resource) => ({
-      url: resource.url,
-      term: extractTermFromText(resource.name),
-    }))
-    .sort((a, b) => b.term.localeCompare(a.term));
-  if (!mostRecentVoteResource) {
-    throw new Error(`No suitable voting resources found`, {
-      cause: votesPackage.result.resources,
-    });
-  }
-  const requestStream = await openDataClient.fetchDataset(
-    mostRecentVoteResource.url,
-  );
-  await deleteAllRawVotes();
-  await bulkInsertRawVotes(
-    formatVoteCsvStream(mostRecentVoteResource.term, requestStream),
-  );
-}
-
-async function repopulateRawContacts(openDataClient: OpenDataClient) {
-  const contactsPackage = await openDataClient.showPackage('contacts');
-  await deleteAllRawContacts();
-  for (const resource of contactsPackage.result.resources) {
-    if (!isCompleteCsvResource(resource)) continue;
-    const term = extractTermFromText(resource.name);
-    const requestStream = await openDataClient.fetchDataset(resource.url);
-    await bulkInsertRawContacts(formatContactCsvStream(term, requestStream));
-  }
-}
 
 async function* asBatches<T>(
   input: AsyncIterable<T>,
@@ -91,25 +141,3 @@ async function* asBatches<T>(
   }
   if (batch.length > 0) yield batch;
 }
-
-async function bulkInsertRawContacts(
-  rowStream: AsyncIterable<InsertRawContact>,
-) {
-  for await (const batch of asBatches(rowStream, INSERT_BATCH_SIZE)) {
-    await db.insertInto('RawContacts').values(batch).execute();
-  }
-}
-
-async function bulkInsertRawVotes(rowStream: AsyncIterable<InsertRawVote>) {
-  for await (const batch of asBatches(rowStream, INSERT_BATCH_SIZE)) {
-    await db.insertInto('RawVotes').values(batch).execute();
-  }
-}
-
-const deleteAllRawContacts = async () => {
-  await sql`TRUNCATE "RawContacts";`.execute(db);
-};
-
-const deleteAllRawVotes = async () => {
-  await sql`TRUNCATE "RawVotes";`.execute(db);
-};
