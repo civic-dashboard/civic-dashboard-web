9d3c23708fc1e86e3cda953762b4a5fbf469ddb0
Milan Somasundaram <sarmilans@gmail.com>
2025-03-01 16:51:58 -0500

    npm version update, .mjs file changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.toronto.ca',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+    domains: ['contrib.wp.intra.prod-toronto.ca'],
+  },
+};
 
 export default nextConfig;
