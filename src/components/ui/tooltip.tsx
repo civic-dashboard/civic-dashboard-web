3f2fa4c708203739ee61cbab3c0219250a0e7ec5
Milan Somasundaram <sarmilans@gmail.com>
2025-03-11 18:11:04 -0400

    replace x with X lucide icon

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { X } from 'lucide-react';
 
 interface TooltipProps {
   children: React.ReactNode;
@@ -49,7 +50,7 @@ export function Tooltip({
                 className="absolute -right-[11px] -top-[11px] rounded-full w-6 h-6 flex items-center justify-center text-gray-500 hover:text-gray-700"
                 aria-label="Close tooltip"
               >
-                ✕
+                <X />
               </button>
               <div className="font-bold italic text-[20px] mb-[10px]">
                 {tooltipTitle}:
