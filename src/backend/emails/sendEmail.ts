539930e040d7f8bb5d7e35bbdfc77767b95f556c
shrouxm <mushrouxm@gmail.com>
2025-03-19 14:45:59 -0700

    dx: email previews in development

--- src/backend/emails/sendEmail.ts
+++ src/backend/emails/sendEmail.ts
@@ -1,8 +1,28 @@
-import { CreateEmailOptions, Resend } from 'resend';
+import { Resend } from 'resend';
+import { render } from '@react-email/components';
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail(options: CreateEmailOptions) {
+type Options = {
+  from: string;
+  subject: string;
+  to: string;
+  react: React.ReactElement;
+};
+export async function sendEmail(options: Options) {
+  if (process.env.NODE_ENV !== 'production') {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    const nodemailer = await import('nodemailer');
+    const transport = nodemailer.createTransport({
+      host: 'localhost',
+      port: 1025,
+    });
+
+    const { react, ...emailOptions } = options;
+    await transport.sendMail({ ...emailOptions, html: await render(react) });
+    return;
+  }
+
   const response = await resend.emails.send(options);
 
   if (response.error) {
