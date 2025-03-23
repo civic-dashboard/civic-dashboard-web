5d05aa22b525368e2e21358c468686bcf3ac902d
Matthew Orchard <23462213+mattorchard@users.noreply.github.com>
2025-03-23 18:07:41 -0400

    Councillor list page updates (#76)
    - Adds filter
    - Adds second column
    - Updates link to a better councillor link
    - Uses Next image instead of regular image and swaps to round avatars

--- src/app/councillors/page.tsx
+++ src/app/councillors/page.tsx
@@ -1,6 +1,8 @@
+'use server';
 import { createDB } from '@/database/kyselyDb';
-import Link from 'next/link';
 import { ExternalLink } from '@/components/ExternalLink';
+import { CouncillorsList } from '@/components/CouncillorsList';
+import { sql } from 'kysely';
 
 async function listCouncillors() {
   return await createDB()
@@ -16,63 +18,43 @@ async function listCouncillors() {
       'Contacts.contactName',
       'Contacts.photoUrl',
       'Wards.wardName',
+      sql<string>`
+      LOWER(
+        CONCAT_WS(
+          ', ',
+          "Contacts"."contactName",
+          "Wards"."wardName",
+          "Wards"."wardId"
+        )
+      )
+    `.as('searchTarget'),
     ])
     .orderBy(['Contacts.contactName'])
     .execute();
 }
 
-const wardProfilesLink =
-  'https://www.toronto.ca/city-government/data-research-maps/neighbourhoods-communities/ward-profiles/';
+const membersOfCouncilLink =
+  'https://www.toronto.ca/city-government/council/members-of-council/';
 
 export default async function CouncillorListPage() {
   const councillors = await listCouncillors();
+
   return (
-    <div className="max-w-screen-sm mx-auto mt-3">
+    <div className="max-w-screen-sm mx-auto my-4 px-2">
       <section className="mt-4">
         <h2>Find Your Councillor</h2>
         <p>
           Not sure who your councillor is? No problem!{' '}
-          <ExternalLink href={wardProfilesLink} className="underline">
-            The ward profiles page
+          <ExternalLink href={membersOfCouncilLink} className="classic-link">
+            The Members of Council page
           </ExternalLink>{' '}
           can show what <em>ward</em> you are a part of. Each councillor serves
           one ward, and all Toronto wards are listed here.
         </p>
       </section>
       <section className="mt-4">
-        <h2>Current Toronto Councillors</h2>
-        <ul>
-          {councillors.map((councillor) => (
-            <li
-              key={councillor.contactSlug}
-              className="border border-slate-200 bg-white even:bg-slate-50 text-slate-950 shadow-sm dark:border-slate-800 dark:bg-slate-950 even:dark:bg-slate-900 dark:text-slate-50 rounded-md mb-2"
-            >
-              <Link
-                href={`/councillors/${councillor.contactSlug}`}
-                className="p-2 flex gap-2 hover:underline"
-                prefetch={false}
-              >
-                <CouncillorContactPhoto photoUrl={councillor.photoUrl} />
-                <div>
-                  <h3 className="text-lg">{councillor.contactName}</h3>
-                  <p>{councillor.wardName}</p>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <CouncillorsList councillors={councillors} />
       </section>
     </div>
-  );
-}
-
-function CouncillorContactPhoto(props: { photoUrl: string | null }) {
-  return (
-    <img
-      src={props.photoUrl ?? ''}
-      className="bg-zinc-300"
-      style={{ height: 52, width: 42 }}
-      alt=""
-    />
   );
 }
