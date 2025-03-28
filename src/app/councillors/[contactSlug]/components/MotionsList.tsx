b08a2e04e0c031d62656e6ff2fd5b4929b45ea59
Matt Orchard <morch028@uottawa.ca>
2025-03-28 17:36:50 -0400

    DLify

--- src/app/councillors/[contactSlug]/components/MotionsList.tsx
+++ src/app/councillors/[contactSlug]/components/MotionsList.tsx
@@ -6,7 +6,7 @@ import {
   CircleDotDashedIcon,
 } from 'lucide-react';
 
-const getVoteIcon = (value: string) => {
+const VoteIcon = ({ value }: { value: string }) => {
   switch (value?.toLowerCase()) {
     case 'yes':
       return <ThumbsUpIcon className="w-4 h-4" />;
@@ -28,45 +28,39 @@ export const MotionsList = ({ motions }: MotionsListProps) => {
     <div className="mt-2">
       {motions.map((motion) => (
         <div key={motion.motionId} className="border-t p-4">
-          <div className="flex justify-between items-center mb-2 text-xs">
-            <div>
-              <span>Date</span>{' '}
-              <span className="text-gray-500">{motion.dateTime}</span>
-            </div>
-            <div className="font-medium">
-              <span>Motion</span>{' '}
-              <span className="text-gray-500">{motion.motionType}</span>
-            </div>
-          </div>
+          <dl className="flex -center mb-2 text-xs gap-1">
+            <dt>Date</dt>
+            <dd className="text-gray-500">{motion.dateTime}</dd>
+            <dt className="ml-auto">Motion</dt>
+            <dd className="text-gray-500">{motion.motionType}</dd>
+          </dl>
 
-          <div className="flex flex-row gap-4 justify-between items-center text-xs">
+          <dl className="flex flex-row gap-4 justify-between items-center text-xs">
             <div className="flex items-center">
-              <div className="flex flex-row">
-                <div className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-[#1870F8] text-white text-md">
-                  {getVoteIcon(motion.value)}
-                </div>
-                <div className="pl-2">
-                  <div>Vote</div>
-                  <div className="text-gray-500">{motion.value}</div>
-                </div>
-              </div>
+              <dt className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-[#1870F8] text-white text-md">
+                <VoteIcon value={motion.value} />
+              </dt>
+              <dd className="pl-2">
+                <div>Vote</div>
+                <div className="text-gray-500">{motion.value}</div>
+              </dd>
             </div>
 
             <div className="flex flex-col items-center text-center">
-              <div>Decision body</div>
-              <div className="text-gray-500">{motion.committeeName}</div>
+              <dt>Decision body</dt>
+              <dd className="text-gray-500">{motion.committeeName}</dd>
             </div>
 
             <div className="flex flex-col items-center text-center">
-              <div>Total</div>
-              <div className="text-gray-500">{motion.tally}</div>
+              <dt>Total</dt>
+              <dd className="text-gray-500">{motion.tally}</dd>
             </div>
 
             <div className="flex flex-col items-center text-center">
-              <div>Status</div>
-              <div className="text-gray-500">{motion.resultKind}</div>
+              <dt>Status</dt>
+              <dd className="text-gray-500">{motion.resultKind}</dd>
             </div>
-          </div>
+          </dl>
         </div>
       ))}
     </div>
