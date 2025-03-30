83f909bb528c83dd47b621322e85e60f0cd31cc3
Matt Orchard <morch028@uottawa.ca>
2025-03-30 16:50:56 -0400

    Simplify panel code flow

--- src/app/councillors/[contactSlug]/components/SummaryPanel.tsx
+++ src/app/councillors/[contactSlug]/components/SummaryPanel.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { cn } from '@/components/ui/utils';
 import { sanitize } from '@/logic/sanitize';
 import { SparklesIcon } from 'lucide-react';
-import { FC, useLayoutEffect, useRef, useState } from 'react';
+import React, { FC, memo, useLayoutEffect, useRef, useState } from 'react';
 
 interface SummaryPanelProps {
   originalSummary: string;
@@ -15,17 +15,21 @@ export const SummaryPanel: FC<SummaryPanelProps> = ({
   originalSummary,
   aiSummary,
 }) => {
-  const [isExpanded, setIsExpanded] = useState(!!aiSummary);
-  const [isExpansionNeeded, setIsExansionNeeded] = useState(false);
   const [tab, setTab] = useState<TabKind>(aiSummary ? 'ai' : 'original');
 
-  const isExpansionToggleVisible =
-    !aiSummary && (isExpansionNeeded || isExpanded);
+  if (!aiSummary) {
+    return (
+      <ExpandableText>
+        <SafeSummary summaryHtml={originalSummary} />
+      </ExpandableText>
+    );
+  }
+
   return (
     <div>
-      {tab === 'ai' && aiSummary ? (
-        <div>
-          <div className="text-sm mb-2">
+      {tab === 'ai' ? (
+        <>
+          <div className="text-sm">
             {/* Todo: Determine if we can omit this header from the prompt */}
             <Markdown>{aiSummary.replace('**Context**', '')}</Markdown>
           </div>
@@ -39,22 +43,11 @@ export const SummaryPanel: FC<SummaryPanelProps> = ({
               Show original
             </Button>
           </footer>
-        </div>
+        </>
       ) : (
         <>
-          <div className="text-sm mb-2">
-            <ExpandableText
-              isExpanded={isExpanded}
-              onExpansionNeededChanged={setIsExansionNeeded}
-            >
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: sanitize(originalSummary),
-                }}
-              />
-            </ExpandableText>
-          </div>
-          <footer className="flex h-10">
+          <SafeSummary summaryHtml={originalSummary} />
+          <footer className="flex h-10 mt-2">
             {aiSummary && (
               <Button
                 variant="ghost"
@@ -65,15 +58,6 @@ export const SummaryPanel: FC<SummaryPanelProps> = ({
                 Show AI Summary
               </Button>
             )}
-            {isExpansionToggleVisible && (
-              <Button
-                onClick={() => setIsExpanded(!isExpanded)}
-                variant="ghost"
-                className="font-bold ml-auto"
-              >
-                {isExpanded ? 'Show less' : 'Show more'}
-              </Button>
-            )}
           </footer>
         </>
       )}
@@ -81,26 +65,53 @@ export const SummaryPanel: FC<SummaryPanelProps> = ({
   );
 };
 
-const ExpandableText: FC<{
-  isExpanded: boolean;
-  onExpansionNeededChanged: (isExpansionNeeded: boolean) => void;
-  children: React.ReactNode;
-}> = ({ isExpanded, onExpansionNeededChanged, children }) => {
-  const ref = useRef<HTMLDivElement>(undefined!);
+const SafeSummary = memo(function SafeSummary({
+  summaryHtml,
+}: {
+  summaryHtml: string;
+}) {
+  return (
+    <div
+      className="text-sm rich-html-styles"
+      dangerouslySetInnerHTML={{
+        __html: sanitize(summaryHtml),
+      }}
+    />
+  );
+});
+
+const ExpandableText: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpansionNeeded, setIsExansionNeeded] = useState(false);
 
   useLayoutEffect(() => {
     const element = ref.current;
     if (!element) return;
-    onExpansionNeededChanged(element.scrollHeight > element.clientHeight);
-  });
+    setIsExansionNeeded(element.scrollHeight > element.clientHeight);
+  }, []);
+
   return (
-    <div
-      ref={ref}
-      className={cn('rich-html-styles overflow-hidden', {
-        'max-h-24 line-clamp-5 text-ellipsis': !isExpanded,
-      })}
-    >
-      {children}
+    <div>
+      <div
+        ref={ref}
+        className={cn('rich-html-styles overflow-hidden', {
+          'max-h-24 line-clamp-5 text-ellipsis': !isExpanded,
+        })}
+      >
+        {children}
+      </div>
+      <footer className="flex h-10 justify-end mt-2">
+        {(isExpansionNeeded || isExpanded) && (
+          <Button
+            onClick={() => setIsExpanded(!isExpanded)}
+            variant="ghost"
+            className="font-bold ml-auto"
+          >
+            {isExpanded ? 'Show less' : 'Show more'}
+          </Button>
+        )}
+      </footer>
     </div>
   );
 };
--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -3,9 +3,12 @@
 // eslint-disable-next-line no-restricted-imports
 import RemarkMarkdown from 'react-markdown';
 
-// Todo: Block in ESLint
 interface MarkdownProps {
   children: string;
 }
 
-export const Markdown = (props: MarkdownProps) => <RemarkMarkdown {...props} />;
+export const Markdown = (props: MarkdownProps) => (
+  <div className="rich-html-styles">
+    <RemarkMarkdown {...props} />
+  </div>
+);
