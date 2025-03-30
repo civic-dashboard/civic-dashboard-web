9514bffec6d776a8c1a0796065686329702f1a83
Matt Orchard <morch028@uottawa.ca>
2025-03-30 15:31:28 -0400

    Add toggling between AI summaries and original content and hide show more when everything shown

--- src/app/councillors/[contactSlug]/components/SummaryPanel.tsx
+++ src/app/councillors/[contactSlug]/components/SummaryPanel.tsx
@@ -1,8 +1,11 @@
 'use client';
+import { AiIndicator } from '@/components/AiIndicator';
 import { Markdown } from '@/components/Markdown';
+import { Button } from '@/components/ui/button';
 import { cn } from '@/components/ui/utils';
 import { sanitize } from '@/logic/sanitize';
-import { FC, useState } from 'react';
+import { SparklesIcon } from 'lucide-react';
+import { FC, useLayoutEffect, useRef, useState } from 'react';
 
 interface SummaryPanelProps {
   originalSummary: string;
@@ -13,34 +16,84 @@ export const SummaryPanel: FC<SummaryPanelProps> = ({
   aiSummary,
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [tab] = useState<TabKind>(aiSummary ? 'ai' : 'original');
+  const [isExpansionNeeded, setIsExansionNeeded] = useState(false);
+  const [tab, setTab] = useState<TabKind>(aiSummary ? 'ai' : 'original');
+
   return (
     <div>
       {tab === 'ai' && aiSummary ? (
         <div>
-          <Markdown>{aiSummary}</Markdown>
-          <p>AI summary</p>
+          <Markdown>{aiSummary.replace('**Context**', '')}</Markdown>
+          <footer className="flex justify-between mt-2">
+            <AiIndicator />
+            <Button
+              variant="ghost"
+              className="font-bold"
+              onClick={() => setTab('original')}
+            >
+              Show original
+            </Button>
+          </footer>
         </div>
       ) : (
         <>
-          <div
-            className={cn('text-sm mb-2 rich-html-styles overflow-hidden', {
-              'max-h-24 line-clamp-5 text-ellipsis': !isExpanded,
-            })}
-            dangerouslySetInnerHTML={{
-              __html: sanitize(originalSummary),
-            }}
-          />
-          <div className="flex justify-end">
-            <button
-              onClick={() => setIsExpanded(!isExpanded)}
-              className="text-sm cursor-pointer font-semibold"
-            >
-              {isExpanded ? 'Show less' : 'Show more'}
-            </button>
-          </div>
+          <ExpandableText
+            isExpanded={isExpanded}
+            onExpansionNeededChanged={setIsExansionNeeded}
+          >
+            <div
+              dangerouslySetInnerHTML={{
+                __html: sanitize(originalSummary),
+              }}
+            />
+          </ExpandableText>
+          <footer className="flex">
+            {aiSummary && (
+              <Button
+                variant="ghost"
+                className="font-bold"
+                onClick={() => setTab('ai')}
+              >
+                <SparklesIcon />
+                Show AI Summary
+              </Button>
+            )}
+            {(isExpansionNeeded || isExpanded) && (
+              <Button
+                onClick={() => setIsExpanded(!isExpanded)}
+                variant="ghost"
+                className="font-bold ml-auto"
+              >
+                {isExpanded ? 'Show less' : 'Show more'}
+              </Button>
+            )}
+          </footer>
         </>
       )}
+    </div>
+  );
+};
+
+const ExpandableText: FC<{
+  isExpanded: boolean;
+  onExpansionNeededChanged: (isExpansionNeeded: boolean) => void;
+  children: React.ReactNode;
+}> = ({ isExpanded, onExpansionNeededChanged, children }) => {
+  const ref = useRef<HTMLDivElement>(undefined!);
+
+  useLayoutEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+    onExpansionNeededChanged(element.scrollHeight > element.clientHeight);
+  });
+  return (
+    <div
+      ref={ref}
+      className={cn('text-sm mb-2 rich-html-styles overflow-hidden', {
+        'max-h-24 line-clamp-5 text-ellipsis': !isExpanded,
+      })}
+    >
+      {children}
     </div>
   );
 };
