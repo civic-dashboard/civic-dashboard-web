742ed4070fbfef76f5ded2741573f7fc2af5fdbe
Milan Somasundaram <sarmilans@gmail.com>
2025-03-29 20:56:44 -0400

    use Link in stead of onClick, auto collapse when on Feedback page

--- src/components/navigation/NotificationBanner.tsx
+++ src/components/navigation/NotificationBanner.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function NotificationBanner({
   message,
@@ -9,22 +10,23 @@ export default function NotificationBanner({
   message: string;
   link: string;
 }) {
+  const pathname = usePathname();
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const router = useRouter();
 
-  const handleBannerClick = () => {
-    router.push(link);
-  };
+  useEffect(() => {
+    if (pathname === link) {
+      setIsCollapsed(true);
+    }
+  }, [pathname, link]);
 
   return (
     <div className="fixed w-full top-16 z-50">
       {!isCollapsed ? (
-        <div className="bg-orange-400 text-white w-full">
-          <div
-            className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2 flex items-center justify-between cursor-pointer"
-            onClick={handleBannerClick}
-          >
-            <p className="text-md font-semibold">{message}</p>
+        <div className="bg-orange-400 text-white">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2 flex items-center justify-between">
+            <Link href={link} className="w-fit">
+              <p className="text-md font-semibold">{message}</p>
+            </Link>
             <button
               onClick={(e) => {
                 e.stopPropagation();
