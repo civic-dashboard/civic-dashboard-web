7a5d5ce962ede19e35b8e375acedf13e22dbc8f4
Matt Orchard <morch028@uottawa.ca>
2025-03-30 15:30:27 -0400

    Block unwrapped usages of react-markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,3 +1,6 @@
+// This file wraps react-markdown so we can ensure we configure it consistently
+// When rendering markdown use this file instead of react-markdown directly
+// eslint-disable-next-line no-restricted-imports
 import RemarkMarkdown from 'react-markdown';
 
 // Todo: Block in ESLint
