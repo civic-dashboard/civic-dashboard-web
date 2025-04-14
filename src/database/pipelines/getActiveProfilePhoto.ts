cfb35a586495d239b6f1603d8813026b4b8b6fc0
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2025-04-13 20:54:11 -0400

    Update scraper to check if Ward Name matches and expanded selector to search for class and id

--- src/database/pipelines/getActiveProfilePhoto.ts
+++ src/database/pipelines/getActiveProfilePhoto.ts
@@ -2,24 +2,42 @@ import * as cheerio from 'cheerio';
 
 // NOTE:  Can only scrape active councillors, not past ones
 // TODO: place file somewhere better, maybe @api/cityCouncilRequest.ts or sanitize.ts
-function parseWardHTML(html: string, wardNum: number) {
+function parseWardHTML(
+  html: string,
+  wardNum: number | string,
+  wardName: string,
+) {
+
     const $ = cheerio.load(html);
 
-    const name: string = $('#page-header--title').text().trim() || `Ward ${wardNum}`;
-    const imgUrl: string | undefined = $('#page-content > div > p > img').attr('src');
+    const headerName: string = $('#page-header--title').text().trim() || `Ward ${wardNum}`;
+    
+    const wardNameToMatch = wardName.toLowerCase().split(/\s+/)
+    const wardHeaderNameToMatch = headerName.trim().toLowerCase()
+    const isWardNameMatch = wardNameToMatch.some(word => wardHeaderNameToMatch.includes(word))
+    
+    if(!isWardNameMatch){
+        console.warn(`Warning: "${wardName}" does not match site councillor header name "${headerName}".`);
+        return null;
+    }
+    
+    const imgUrl: string | undefined = $('#page-content > div > p > img', '.page-content > div > p > img').attr('src');
 
     if (imgUrl) {
-        console.log(`Retrieved image src URL for ${name}`);
+        console.log(`Retrieved image src URL for ${headerName}`);
     } else {
-        console.log(`No image found for ${name}`);
+        console.log(`No image found for ${headerName}`);
     }
 
     return imgUrl;
 }
 
 
-export const getMemberSitePortrait = async (wardNum: number) => {
-
+export async function getMemberSitePortrait (
+  wardNum: number | string,
+  wardName: string,
+) {
+    
     const URL: string = `https://www.toronto.ca/city-government/council/members-of-council/councillor-ward-${wardNum}/`;
 
     try {
@@ -28,7 +46,7 @@ export const getMemberSitePortrait = async (wardNum: number) => {
 
         console.log('Retriving img url for ward:', wardNum); 
 
-        const imgUrl = parseWardHTML(HTML, wardNum); 
+        const imgUrl = parseWardHTML(HTML, wardNum, wardName); 
 
         if (imgUrl) {
             console.log(`Retrieved image URL for ward ${wardNum}: ${imgUrl}`);
@@ -37,6 +55,6 @@ export const getMemberSitePortrait = async (wardNum: number) => {
     } catch (error) {
         console.error('Error fetching img url for ward:', wardNum, error);
     }
-};
+    };
 
 
