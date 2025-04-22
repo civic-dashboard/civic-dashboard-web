a92c45184328ac8ce3a944f1c78d65333e0fcc48
Melissa Luc <melissa.ml.luc@gmail.com>
2025-04-22 14:41:57 -0400

    Refactored rawContactsCsvParser to batch process rows

--- src/database/pipelines/rawContactCsvParser.ts
+++ src/database/pipelines/rawContactCsvParser.ts
@@ -45,56 +45,90 @@ type CsvContactRow = CsvRow<typeof ContactCsvColumns>;
 
 export const formatContactCsvStream = (
   term: string,
-  requestStream: Readable,
-): AsyncIterable<InsertRawContact> =>
-  requestStream
-    .pipe(
-      createCsvParser({
-        trim: true,
-        cast: sharedCast,
-        columns: createColumnMapper(ContactCsvColumns),
-      }),
-    )
-    // Vacancies appear with nullish names
-    .filter((row: CsvContactRow) => Boolean(row.firstName && row.lastName))
-    .map(async (row: CsvContactRow): InsertRawContact => {
-      if (!verifyFieldsAreNotNullish(MandatoryContactFields, row)) {
-        throw new Error(`Missing not-nullable field(s)`, { cause: { row } });
-      }
-      const contactName = toContactName(row.firstName, row.lastName);
+  requestStream: Readable
+): AsyncIterable<InsertRawContact> => {
+  const parsedRequestStream = requestStream.pipe(
+    createCsvParser({
+      trim: true,
+      cast: sharedCast,
+      columns: createColumnMapper(ContactCsvColumns),
+    })
+  );
+  // Vacancies appear with nullish names
+  const filteredRequestStream: AsyncIterable<CsvContactRow> =
+    parsedRequestStream.filter((row: CsvContactRow) =>
+      Boolean(row.firstName && row.lastName)
+    );
+  const rowBatch: Array<CsvContactRow> = [];
+  // TODO: Adjust this based on constraints or add logic to determine batch size dynamically
+  const batchSize = 26;
+  return processRowsInBatches(filteredRequestStream, batchSize, (row) =>
+    processRow(row, term)
+  );
+};
 
-      let imgUrl: string | null = row.photoUrl ?? null;
-      if (!(await validateUrl(imgUrl))) {
-        console.log(
-          `Invalid photo URL for council member ${contactName}:`,
-          imgUrl,
-        );
-        imgUrl = await getMemberSitePortrait(
-          row.districtId,
-          contactName,
-          row.primaryRole,
-        );
-      }
+// Process rows and validates fields
+async function processRow(row: CsvContactRow, term: string): InsertRawContact {
+  if (!verifyFieldsAreNotNullish(MandatoryContactFields, row)) {
+    throw new Error(`Missing not-nullable field(s)`, { cause: { row } });
+  }
+  const contactName = toContactName(row.firstName, row.lastName);
 
-      return {
-        term,
-        contactName,
-        contactSlug: toSlug(contactName),
-        wardSlug: toSlug(row.districtName),
-        inputRowNumber: row.id,
-        wardId: row.districtId,
-        wardName: row.districtName,
-        primaryRole: row.primaryRole,
-        email: row.email,
-        photoUrl: imgUrl,
-        website: row.website,
-        addressLine1: row.addressLine1,
-        addressLine2: row.addressLine2,
-        locality: row.locality,
-        postalCode: row.postalCode,
-        province: row.province,
-        phone: row.phone,
-        personalWebsite: row.personalWebsite,
-        fax: row.fax,
-      };
-    });
+  let imgUrl: string | null = row.photoUrl ?? null;
+  if (!(await validateUrl(imgUrl))) {
+    console.log(`Invalid photo URL for council member ${contactName}:`, imgUrl);
+    imgUrl = await getMemberSitePortrait(
+      row.districtId,
+      contactName,
+      row.primaryRole
+    );
+  }
+
+  return {
+    term,
+    contactName,
+    contactSlug: toSlug(contactName),
+    wardSlug: toSlug(row.districtName),
+    inputRowNumber: row.id,
+    wardId: row.districtId,
+    wardName: row.districtName,
+    primaryRole: row.primaryRole,
+    email: row.email,
+    photoUrl: imgUrl,
+    website: row.website,
+    addressLine1: row.addressLine1,
+    addressLine2: row.addressLine2,
+    locality: row.locality,
+    postalCode: row.postalCode,
+    province: row.province,
+    phone: row.phone,
+    personalWebsite: row.personalWebsite,
+    fax: row.fax,
+  };
+}
+
+async function* processRowsInBatches<T>(
+  iterable: AsyncIterable<T>,
+  batchSize: number,
+  processFunction: (item: T) => Promise<InsertRawContact>
+): AsyncIterable<InsertRawContact> {
+  const batch: Array<T> = [];
+  async function* processBatch() {
+    if (batch.length > 0) {
+      for (const row of await Promise.all(batch.map(processFunction))) {
+        yield row;
+      }
+    }
+  }
+  for await (const item of iterable) {
+    batch.push(item);
+    if (batch.length >= batchSize) {
+      yield* processBatch();
+    }
+  }
+
+  // Process remaining rows, where batch length is less than batchSize
+  if (batch.length > 0) {
+    yield* processBatch();
+  }
+}
