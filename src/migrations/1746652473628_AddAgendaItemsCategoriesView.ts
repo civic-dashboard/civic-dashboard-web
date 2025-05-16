9cdd1d77c05bf93682c967ac92425ebea1b9e34a
Melissa Luc <melissa.ml.luc@gmail.com>
2025-05-15 20:56:43 -0400

    Refactor SQL syntax in AgendaItemsCategories materialized view creation

--- src/migrations/1746652473628_AddAgendaItemsCategoriesView.ts
+++ src/migrations/1746652473628_AddAgendaItemsCategoriesView.ts
@@ -2,18 +2,17 @@ import { sql, type Kysely } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
   await sql`
-  CREATE MATERIALIZED VIEW "AgendaItemsCategories" AS
+ CREATE MATERIALIZED VIEW "AgendaItemsCategories" AS
   SELECT DISTINCT
       t1."agendaItemId",
       t2."category"
   FROM "RawAgendaItemConsiderations" t1
   JOIN "TagCategories" t2 
-      ON t2."tag" = ANY(STRING_TO_ARRAY(t1."subjectTerms", ','));
+      ON t2."tag" = ANY(STRING_TO_ARRAY(t1."subjectTerms", ','))
+  ORDER BY t1."agendaItemId", t2."category";
   CREATE UNIQUE INDEX ON "AgendaItemsCategories"("agendaItemId", "category");
-
 	`.execute(db);
 }
-//join with RawAgendaItemConsiderations on subjectterms
 
 export async function down(db: Kysely<any>): Promise<void> {
   await sql`
