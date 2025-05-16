43928a91ebd329d752fb19001041b03d725691c5
Melissa Luc <melissa.ml.luc@gmail.com>
2025-05-15 21:29:07 -0400

    Ran eslint and prettier

--- src/migrations/1746652473628_AddAgendaItemsCategoriesView.ts
+++ src/migrations/1746652473628_AddAgendaItemsCategoriesView.ts
@@ -1,21 +1,24 @@
 import { sql, type Kysely } from 'kysely';
 
-export async function up(db: Kysely<any>): Promise<void> {
+export async function up(db: Kysely<unknown>): Promise<void> {
   await sql`
- CREATE MATERIALIZED VIEW "AgendaItemsCategories" AS
-  SELECT DISTINCT
+    CREATE MATERIALIZED VIEW "AgendaItemsCategories" AS
+    SELECT DISTINCT
       t1."agendaItemId",
       t2."category"
-  FROM "RawAgendaItemConsiderations" t1
-  JOIN "TagCategories" t2 
-      ON t2."tag" = ANY(STRING_TO_ARRAY(t1."subjectTerms", ','))
-  ORDER BY t1."agendaItemId", t2."category";
-  CREATE UNIQUE INDEX ON "AgendaItemsCategories"("agendaItemId", "category");
-	`.execute(db);
+    FROM
+      "RawAgendaItemConsiderations" t1
+      JOIN "TagCategories" t2 ON t2."tag" = ANY (STRING_TO_ARRAY(t1."subjectTerms", ','))
+    ORDER BY
+      t1."agendaItemId",
+      t2."category";
+
+    CREATE UNIQUE INDEX ON "AgendaItemsCategories" ("agendaItemId", "category");
+  `.execute(db);
 }
 
-export async function down(db: Kysely<any>): Promise<void> {
+export async function down(db: Kysely<unknown>): Promise<void> {
   await sql`
-  DROP MATERIALIZED VIEW IF EXISTS "AgendaItemsCategories" CASCADE;
-`.execute(db);
+    DROP MATERIALIZED VIEW IF EXISTS "AgendaItemsCategories" CASCADE;
+  `.execute(db);
 }
