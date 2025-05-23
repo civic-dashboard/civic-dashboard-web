0aeed6a7ca0212d5c1dd485829fe49c9155fbd52
Melissa Luc <melissa.ml.luc@gmail.com>
2025-05-22 21:47:28 -0400

    Remove redundant try-catch for proper stack trace logging; removed AddAgendaItemsCategoriesView migration due to potential performance issues

--- seeds/1746647286572_AddToTagCategories.ts
+++ seeds/1746647286572_AddToTagCategories.ts
@@ -1,14 +1,10 @@
 import type { Kysely } from 'kysely';
 import fs from 'fs';
 export async function seed(db: Kysely<any>): Promise<void> {
-  try {
-    const rawData = fs.readFileSync('seeds/categories_tags.json', 'utf-8');
-    const jsonData = JSON.parse(rawData);
+  const rawData = fs.readFileSync('seeds/categories_tags.json', 'utf-8');
+  const jsonData = JSON.parse(rawData);
 
-    await db.deleteFrom('TagCategories').execute();
-    await db.insertInto('TagCategories').values(jsonData).execute();
-    console.log('Seeded TagCategories table successfully');
-  } catch (error) {
-    console.error('Error reading or parsing JSON file:', error);
-  }
+  await db.deleteFrom('TagCategories').execute();
+  await db.insertInto('TagCategories').values(jsonData).execute();
+  console.log('Seeded TagCategories table successfully');
 }
