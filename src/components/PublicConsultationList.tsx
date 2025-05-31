a467065f25231e29df53881d2b97f8232b3a9290
Caroline Liu <17cliu@users.noreply.github.com>
2025-05-30 22:01:30 -0400

    Add error handling to Public Consultation page (#176)
    While working on #175, the CI could not build the site -- it seemed to
    break on the public consultation page, because the fetched data was
    invalid JSON. However, the page loaded fine when run locally, so I
    assume that the API just didn't like the CI fetching from it.
    
    This PR adds some error handling to the public consultation list so it
    doesn't crash if it fails to fetch data.

--- src/components/PublicConsultationList.tsx
+++ src/components/PublicConsultationList.tsx
@@ -6,14 +6,28 @@ const isUpcoming = (event: EventData) => {
 };
 
 export async function PublicConsultationList() {
-  const events = await fetchPublicConsultations();
+  let events: EventData[] = [];
+
+  try {
+    events = await fetchPublicConsultations();
+  } catch (err) {
+    console.log('Could not fetch public consultation data', err);
+  }
+
   return (
     <div className="flex-col space-y-4 p-4 max-w-[1000px]">
-      {events?.map(
-        (event) =>
-          isUpcoming(event) && (
-            <PublicConsultationCard key={event.calEvent.recId} event={event} />
-          ),
+      {events.length ? (
+        events.map(
+          (event) =>
+            isUpcoming(event) && (
+              <PublicConsultationCard
+                key={event.calEvent.recId}
+                event={event}
+              />
+            ),
+        )
+      ) : (
+        <p>No events found</p>
       )}
     </div>
   );
