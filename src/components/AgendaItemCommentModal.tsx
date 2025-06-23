ab1291a249619699b5b201e3f0a3b918f4bdf36e
Caroline Liu <17cliu@users.noreply.github.com>
2025-06-23 14:50:21 -0400

    Inline comment/name fields

--- src/components/AgendaItemCommentModal.tsx
+++ src/components/AgendaItemCommentModal.tsx
@@ -66,19 +66,15 @@ export function AgendaItemCommentModal({ agendaItem, decisionBody }: Props) {
     },
   );
   const subject = `My comments for ${agendaItem.reference} on ${formattedDate} ${decisionBody.decisionBodyName}`;
-  const body = `To the City Clerk:
+  const bodyStartParagraphs = [
+    'To the City Clerk:',
+    `Please add my comments to the agenda for the ${formattedDate} ${decisionBody.decisionBodyId} meeting on item ${agendaItem.reference}, ${agendaItem.agendaItemTitle}`,
+    'I understand that my comments and the personal information in this email will form part of the public record and that my name will be listed as a correspondent on agendas and minutes of City Council or its committees. Also, I understand that agendas and minutes are posted online and my name may be indexed by search engines like Google.',
 
-Please add my comments to the agenda for the ${formattedDate} ${decisionBody.decisionBodyId} meeting on item ${agendaItem.reference}, ${agendaItem.agendaItemTitle}
+    'Comments:',
+  ];
+  const closing = `Sincerely,`;
 
-I understand that my comments and the personal information in this email will form part of the public record and that my name will be listed as a correspondent on agendas and minutes of City Council or its committees. Also, I understand that agendas and minutes are posted online and my name may be indexed by search engines like Google.
-
-Comments:
-
-${comment}
-
-Sincerely,
-${commenterName}
-  `;
   return (
     <Dialog>
       {/* TODO: This duplicates styling for DropdownMenuItem... */}
@@ -113,75 +109,53 @@ ${commenterName}
                 value={decisionBody.decisionBodyName}
               />
             </div>
-            <Fieldset>
-              <Label id="commenterName">Your name</Label>
-              <Input
-                id="commenterName"
-                type="text"
-                value={commenterName}
-                onChange={(e) => setCommenterName(e.target.value)}
-              />
-            </Fieldset>
-            <Fieldset>
-              <Label id="comment">
-                Your comments
-                <Tooltip
-                  // @ts-expect-error TODO: thinking about replacing this with an accordion.
-                  // this is too much text for a tooltip.
-                  tooltipContent={
-                    <div className="text-base">
-                      <p>
-                        Describe who you are, what you want, and why you want
-                        it.
-                      </p>
-                      <ul>
-                        <li>
-                          Who you are - your name, where you live (if it’s
-                          relevant), and any relevant communities you are part
-                          of
-                        </li>
-                        <li>
-                          Your relationship to the item - why do you care about
-                          it? How does it affect you? Why do you think it’s
-                          important?
-                        </li>
-                        <li>
-                          What you want - what would you like this committee to
-                          do? Do you want them to vote yes or no on this item?
-                          Do you want them to amend/change it in some way?
-                        </li>
-                      </ul>
-                      <p>
-                        Example:
-                        <br />
-                        My name is Lisa Michaels, I’m a 20 year resident of the
-                        High Park neighbourhood, and I’m a lifelong birder and
-                        animal lover.
-                        <br />
-                        This item is meant to protect wildlife, and yet it will
-                        greatly increase the level of noise in High Park, which
-                        scares and disorients birds, damages the ecosystem, and
-                        makes the park less enjoyable for everyone! Parks are
-                        about bringing people and nature together, and this
-                        would do the opposite.
-                        <br />I ask that this committee either vote No on this
-                        item, or find a way to amend it that does not increase
-                        the level of noise in the park. My family, my birding
-                        group and I will be following this committee’s actions
-                        closely!
-                      </p>
-                    </div>
-                  }
-                >
-                  <InfoIcon />
-                </Tooltip>
-              </Label>
-              <TextArea
-                id="comment"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-              />
-            </Fieldset>
+
+            <Tooltip
+              // @ts-expect-error TODO: thinking about replacing this with an accordion.
+              // this is too much text for a tooltip.
+              tooltipContent={
+                <div className="text-base">
+                  <p>
+                    Describe who you are, what you want, and why you want it.
+                  </p>
+                  <ul>
+                    <li>
+                      Who you are - your name, where you live (if it’s
+                      relevant), and any relevant communities you are part of
+                    </li>
+                    <li>
+                      Your relationship to the item - why do you care about it?
+                      How does it affect you? Why do you think it’s important?
+                    </li>
+                    <li>
+                      What you want - what would you like this committee to do?
+                      Do you want them to vote yes or no on this item? Do you
+                      want them to amend/change it in some way?
+                    </li>
+                  </ul>
+                  <p>
+                    Example:
+                    <br />
+                    My name is Lisa Michaels, I’m a 20 year resident of the High
+                    Park neighbourhood, and I’m a lifelong birder and animal
+                    lover.
+                    <br />
+                    This item is meant to protect wildlife, and yet it will
+                    greatly increase the level of noise in High Park, which
+                    scares and disorients birds, damages the ecosystem, and
+                    makes the park less enjoyable for everyone! Parks are about
+                    bringing people and nature together, and this would do the
+                    opposite.
+                    <br />I ask that this committee either vote No on this item,
+                    or find a way to amend it that does not increase the level
+                    of noise in the park. My family, my birding group and I will
+                    be following this committee’s actions closely!
+                  </p>
+                </div>
+              }
+            >
+              <InfoIcon />
+            </Tooltip>
           </div>
           <div className="flex flex-col grow w-full md:w-1/2">
             <div className="flex">
@@ -211,10 +185,35 @@ ${commenterName}
                 <span className="sr-only">Copy email body</span>
               </Button>
             </div>
-            <TextArea
-              className="flex-stretch grow text-base md:text-base"
-              value={body}
-            />
+            {bodyStartParagraphs.map((s, i) => (
+              <p key={i} className="mb-3">
+                {s}
+              </p>
+            ))}
+            <Fieldset>
+              <label className="sr-only" htmlFor="comment">
+                Your comments
+              </label>
+              <TextArea
+                id="comment"
+                placeholder="Your comments"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+            </Fieldset>
+            <p className="mb-3">{closing}</p>
+            <Fieldset>
+              <label className="sr-only" htmlFor="commenterName">
+                Your name
+              </label>
+              <Input
+                id="commenterName"
+                type="text"
+                placeholder="Your name"
+                value={commenterName}
+                onChange={(e) => setCommenterName(e.target.value)}
+              />
+            </Fieldset>
           </div>
         </div>
       </DialogContent>
