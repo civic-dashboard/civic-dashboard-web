866ad6749425576bbca51d604370ae99fe5f9902
Caroline Liu <17cliu@users.noreply.github.com>
2025-07-20 15:51:44 -0400

    Move help into an accordion

--- src/components/AgendaItemCommentModal.tsx
+++ src/components/AgendaItemCommentModal.tsx
@@ -13,6 +13,12 @@ import { Input } from '@/components/ui/input';
 import { TextArea } from '@/components/ui/textarea';
 import { ReactNode, useState } from 'react';
 import { Button } from '@/components/ui/button';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 interface Props {
   agendaItem: AgendaItem;
@@ -65,8 +71,6 @@ export function AgendaItemCommentModal({ agendaItem, decisionBody }: Props) {
     'To the City Clerk:',
     `Please add my comments to the agenda for the ${formattedDate} ${decisionBody.decisionBodyId} meeting on item ${agendaItem.reference}, ${agendaItem.agendaItemTitle}`,
     'I understand that my comments and the personal information in this email will form part of the public record and that my name will be listed as a correspondent on agendas and minutes of City Council or its committees. Also, I understand that agendas and minutes are posted online and my name may be indexed by search engines like Google.',
-
-    'Comments:',
   ];
   const closing = `Sincerely,`;
 
@@ -117,7 +121,7 @@ export function AgendaItemCommentModal({ agendaItem, decisionBody }: Props) {
               <li className="ml-4">Fill out the form</li>
               <li className="ml-4">
                 Use the Copy buttons to copy the text into your email client of
-                choice, and send it off! Or, use the create email button, which
+                choice, and send it off! Or, use the Create Email button, which
                 will automatically open your email client with the text already
                 filled in for you.
               </li>
@@ -163,13 +167,28 @@ export function AgendaItemCommentModal({ agendaItem, decisionBody }: Props) {
 
             <div className="w-full md:w-1/3"></div>
           </div>
-          {/* comments + rest row */}
+          {/* comments row */}
           <div className="flex flex-col md:flex-row mb-6 gap-4">
             <div className="flex flex-col grow w-full md:w-2/3">
               <Fieldset>
-                <label className="sr-only" htmlFor="comment">
-                  Your comments
-                </label>
+                <div className="relative mb-2">
+                  <label htmlFor="comment">Comments:</label>
+                  <Accordion type="multiple">
+                    <AccordionItem
+                      value="comment-help"
+                      className="relative border-none"
+                    >
+                      <AccordionTrigger className="absolute -top-5 right-0">
+                        Help
+                      </AccordionTrigger>
+                      <AccordionContent className="m-0 p-0">
+                        <div className="mt-2 mb-2 border rounded p-4 bg-blue-900/50">
+                          <CommentHelpContent />
+                        </div>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
                 <TextArea
                   id="comment"
                   placeholder="Your comments"
@@ -191,87 +210,91 @@ export function AgendaItemCommentModal({ agendaItem, decisionBody }: Props) {
                   onChange={(e) => setCommenterName(e.target.value)}
                 />
               </Fieldset>
-
-              <p className="text-base text-sm mt-4 mb-2">
-                All done? Use these buttons to copy the text into an email, and
-                send that email to: <b>{decisionBody.email}</b>
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                <Button
-                  size="sm"
-                  onClick={() => copyToClipboard(decisionBody.email!)}
-                >
-                  <CopyIcon />
-                  Copy email: {decisionBody.email}
-                </Button>
-                <Button size="sm" onClick={() => copySubjectText()}>
-                  <CopyIcon />
-                  Copy subject line
-                </Button>
-                <Button size="sm" onClick={() => copyBodyText()}>
-                  <CopyIcon />
-                  Copy email body
-                </Button>
-              </div>
-              <p className="text-base text-sm mt-2 mb-2">
-                Or, use this button, which will start a new email in your email
-                client with all the text prefilled for you:
-              </p>
-              <div className="flex gap-2 mb-4">
-                <Button asChild size="sm">
-                  <a href={makeMailtoLink()}>
-                    <ExternalLink />
-                    Create email (opens your mail client)
-                  </a>
-                </Button>
-              </div>
             </div>
-            <div className="w-full md:w-1/3">
-              <div
-                className="text-base text-sm space-y-2"
-                id="comment-detailed-description"
+            <div className="w-full md:w-1/3"></div>
+          </div>
+          {/* copy/send buttons */}
+          <div className="">
+            <p className="text-base text-sm mt-4 mb-2">
+              All done? Use these buttons to copy the text into an email, and
+              send that email to: <b>{decisionBody.email}</b>
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              <Button
+                size="sm"
+                onClick={() => copyToClipboard(decisionBody.email!)}
               >
-                <p>Describe:</p>
-                <ul className="list-disc">
-                  <li className="ml-4">
-                    <b>Who you are:</b> Your name, where you live (if relevant),
-                    and any relevant communities you are part of
-                  </li>
-                  <li className="ml-4">
-                    <b>Your relationship to the item:</b> Why do you care about
-                    it? How does it affect you? Why do you think it&rsquo;s
-                    important?
-                  </li>
-                  <li className="ml-4">
-                    <b>What you want:</b> What would you like this committee to
-                    do? Do you want them to vote yes or no on this item? Do you
-                    want them to amend/change it in some way?
-                  </li>
-                </ul>
-                <h3 className="font-semibold">Example:</h3>
-                <p>
-                  My name is Lisa Michaels, I&rsquo;m a 20 year resident of the
-                  High Park neighbourhood, and I&rsquo;m a lifelong birder and
-                  animal lover.
-                </p>
-                <p>
-                  This item is meant to protect wildlife, and yet it will
-                  greatly increase the level of noise in High Park, which scares
-                  and disorients birds, damages the ecosystem, and makes the
-                  park less enjoyable for everyone! Parks are about bringing
-                  people and nature together, and this would do the opposite.
-                </p>
-                <p>
-                  I ask that this committee either vote No on this item, or find
-                  a way to amend it that does not increase the level of noise in
-                  the park. My family, my birding group and I will be following
-                  this committee&rsquo;s actions closely!
-                </p>
-              </div>
+                <CopyIcon />
+                Copy email: {decisionBody.email}
+              </Button>
+              <Button size="sm" onClick={() => copySubjectText()}>
+                <CopyIcon />
+                Copy subject line
+              </Button>
+              <Button size="sm" onClick={() => copyBodyText()}>
+                <CopyIcon />
+                Copy email body
+              </Button>
+            </div>
+            <p className="text-base text-sm mt-2 mb-2">
+              Or, use this button, which will start a new email in your email
+              client with all the text prefilled for you:
+            </p>
+            <div className="flex gap-2 mb-4">
+              <Button asChild size="sm">
+                <a href={makeMailtoLink()}>
+                  <ExternalLink />
+                  Create email (opens your mail client)
+                </a>
+              </Button>
             </div>
           </div>
         </div>
       </DialogContent>
     </Dialog>
+  );
+}
+
+function CommentHelpContent() {
+  return (
+    <div
+      className="text-base text-sm space-y-2"
+      id="comment-detailed-description"
+    >
+      <p>Describe:</p>
+      <ul className="list-disc">
+        <li className="ml-4">
+          <b>Who you are:</b> Your name, where you live (if relevant), and any
+          relevant communities you are part of
+        </li>
+        <li className="ml-4">
+          <b>Your relationship to the item:</b> Why do you care about it? How
+          does it affect you? Why do you think it&rsquo;s important?
+        </li>
+        <li className="ml-4">
+          <b>What you want:</b> What would you like this committee to do? Do you
+          want them to vote yes or no on this item? Do you want them to
+          amend/change it in some way?
+        </li>
+      </ul>
+      <h3 className="font-semibold">Example:</h3>
+      <p>
+        My name is Lisa Michaels, I&rsquo;m a 20 year resident of the High Park
+        neighbourhood, and I&rsquo;m a lifelong birder and animal lover.
+      </p>
+      <p>
+        This item is meant to protect wildlife, and yet it will greatly increase
+        the level of noise in High Park, which scares and disorients birds,
+        damages the ecosystem, and makes the park less enjoyable for everyone!
+        Parks are about bringing people and nature together, and this would do
+        the opposite.
+      </p>
+      <p>
+        I ask that this committee either vote No on this item, or find a way to
+        amend it that does not increase the level of noise in the park. My
+        family, my birding group and I will be following this committee&rsquo;s
+        actions closely!
+      </p>
+    </div>
   );
 }
