dfc7a935a34446518574625567631b5309881863
Andre053 <andre.61122@proton.me>
2025-10-26 17:40:17 -0400

    Fixed lint

--- src/components/AgendaItemList.tsx
+++ src/components/AgendaItemList.tsx
@@ -109,36 +109,36 @@ function AgendaItemListInner({ initialSearchParams }: Props) {
 
   return (
     <div className="flex flex-col space-y-4 p-4 items-stretch max-w-full sm:max-w-max-content-width">
-        <div className="mt-4 mb-2">
-          <h1 className="text-2xl font-bold">Actions</h1>
-          <p>
-            Here are agenda items that the City of Toronto will discuss at
-            upcoming meetings. You can provide feedback on these items by
-            submitting comments by email, which will be read at the meeting, or
-            requesting to speak at the meeting live, in person or over video
-            conferencing.
-          </p>
-        </div>
-        <hr />
-        <div className="flex flex-row self-stretch items-center space-x-2">
-          <div className="flex-grow">
-            <SearchBar />
-          </div>
-          <div className="sm:max-w-max-content-width">
-            <SortDropdown />
-          </div>
-        </div>
-        <Tags />
-        <hr />
-        <AdvancedFilters decisionBodies={currentTermDecisionBodies} />
-        <div className="flex flex-row justify-around items-end flex-wrap self-stretch space-x-4 space-y-4">
-          <div className="flex grow justify-between items-end">
-            <ResultCount />
-            <SubscribeToSearchButton />
-          </div>
-        </div>
-        <ResultList />
+      <div className="mt-4 mb-2">
+        <h1 className="text-2xl font-bold">Actions</h1>
+        <p>
+          Here are agenda items that the City of Toronto will discuss at
+          upcoming meetings. You can provide feedback on these items by
+          submitting comments by email, which will be read at the meeting, or
+          requesting to speak at the meeting live, in person or over video
+          conferencing.
+        </p>
       </div>
+      <hr />
+      <div className="flex flex-row self-stretch items-center space-x-2">
+        <div className="flex-grow">
+          <SearchBar />
+        </div>
+        <div className="sm:max-w-max-content-width">
+          <SortDropdown />
+        </div>
+      </div>
+      <Tags />
+      <hr />
+      <AdvancedFilters decisionBodies={currentTermDecisionBodies} />
+      <div className="flex flex-row justify-around items-end flex-wrap self-stretch space-x-4 space-y-4">
+        <div className="flex grow justify-between items-end">
+          <ResultCount />
+          <SubscribeToSearchButton />
+        </div>
+      </div>
+      <ResultList />
+    </div>
   );
 }
 
