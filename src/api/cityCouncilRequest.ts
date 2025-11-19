5ca161d4d2d345881bc07fde4ba7f140721a8132
garo (they/them) <3411715+garobrik@users.noreply.github.com>
2025-11-19 09:05:11 -0800

    fix: city council requests (again) (#293)
    ## Description
    
    <!-- 1. Link the related issue here. -->
    <!-- Replace "Resolves" with "Related to" if this PR should not close
    the issue (e.g. if the issue requires several PRs to complete) -->
    Resolves #290
    
    <!-- 2. Give a high-level description of what this PR does. -->
    <!-- What was the behaviour before? What should it be now? -->
    
    <!-- 3. If this PR results in something changing visually on the site,
    include before/after screenshots. -->
    
    <!-- 4. If you have specific code review directions, include them here.
    -->
    <!-- E.g. "Look at these files first" or "Review one commit at a time"
    -->
    
    ## Testing instructions
    
    <!-- Describe how the reviewer should test this change. -->
    <!-- E.g. Which page should they look at? What should they click? What
    should happen after clicking? -->
    
    ## Checklist
    
    <!-- Ensure you do these tasks before requesting a review! -->
    
    - [x] This PR addresses all requirements described in the issue
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have performed a self-review of my code
    - [x] I have tested these changes in the preview deployment
    - [x] I have made corresponding changes to the documentation (if
    relevant)

--- src/api/cityCouncilRequest.ts
+++ src/api/cityCouncilRequest.ts
@@ -5,43 +5,32 @@ type Args = {
   body?: BodyInit;
 };
 
+const DEFAULT_REQUEST_HEADERS = {
+  'User-Agent':
+    'Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0',
+  Accept: 'application/json, text/plain, */*',
+  'Accept-Language': 'en-US,en;q=0.5',
+  'Sec-Fetch-Dest': 'empty',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  Pragma: 'no-cache',
+  'Cache-Control': 'no-cache',
+};
+
 export const cityCouncilGet = async (url: string) => {
   return await fetch(url, {
     credentials: 'include',
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      Pragma: 'no-cache',
-      'Cache-Control': 'no-cache',
-    },
+    headers: DEFAULT_REQUEST_HEADERS,
     method: 'GET',
     mode: 'cors',
   });
-};
-
-const DEFAULT_COUNCIL_HEADERS = {
-  'User-Agent':
-    'Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0',
-  Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'en-US,en;q=0.5',
-  'Upgrade-Insecure-Requests': '1',
-  'Sec-Fetch-Dest': 'document',
-  'Sec-Fetch-Mode': 'navigate',
-  'Sec-Fetch-Site': 'none',
-  'Sec-Fetch-User': '?1',
-  Priority: 'u=0, i',
-  Pragma: 'no-cache',
-  'Cache-Control': 'no-cache',
 };
 
 export const cityCouncilXSRFPost = async ({ url, body }: Args) => {
   // for some reason next's fetch does not receive the XSRF token
   const csrfResponse = await nodeFetch(
     'https://secure.toronto.ca/council/api/csrf.json',
-    { headers: DEFAULT_COUNCIL_HEADERS },
+    { headers: DEFAULT_REQUEST_HEADERS },
   );
 
   if (!csrfResponse.ok) {
@@ -65,14 +54,8 @@ export const cityCouncilXSRFPost = async ({ url, body }: Args) => {
 
   return await fetch(url, {
     headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Accept-Language': 'en-US,en;q=0.5',
+      ...DEFAULT_REQUEST_HEADERS,
       'Content-Type': 'application/json',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      Pragma: 'no-cache',
-      'Cache-Control': 'no-cache',
       'X-XSRF-TOKEN': xsrfToken,
       Cookie: cookies,
     },
