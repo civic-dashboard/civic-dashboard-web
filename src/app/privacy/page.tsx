3ce869b3e38f1a6b4ef49a092ca2216d225fdbfc
Caroline Liu <17cliu@users.noreply.github.com>
2026-02-24 19:14:26 -0500

    Update Privacy Policy page to use design system components (#345)
    ## Description
    
    <!-- 1. Link the related issue here. -->
    <!-- Replace "Resolves" with "Related to" if this PR should not close
    the issue (e.g. if the issue requires several PRs to complete) -->
    Resolves #334
    
    <!-- 2. Give a high-level description of what this PR does. -->
    
    This PR updates the [Privacy Policy](https://civicdashboard.ca/privacy)
    page to use our design system components, so that its styling matches
    the rest of our site. This will also fix this page appearing with black
    text on white background, even in dark mode.
    
    <!-- 3. If this PR results in something changing visually on the site,
    include before/after screenshots. -->
    
    | Before | After |
    |-|-|
    |<img width="1668" height="1255" alt="image"
    src="https://github.com/user-attachments/assets/464ae7db-692b-4ca9-b5b7-11044a41877d"
    />|<img width="1672" height="1228" alt="image"
    src="https://github.com/user-attachments/assets/83430669-ee9f-4f34-b4ae-d32b1fa38041"
    />
    
    I recommend reviewing the code with whitespace hidden.
    
    
    ## Testing instructions
    
    - Visit `/privacy` on the preview site
    - Verify it looks good...
      - at various screen sizes/widths
    - in dark vs light mode (this is a browser and/or OS setting you can
    toggle)
    
    ## Checklist
    
    <!-- Ensure you do these tasks before requesting a review! -->
    
    - [x] This PR addresses all requirements described in the issue
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have performed a self-review of my code
    - [x] I have tested these changes in the preview deployment
    - [x] I have made corresponding changes to the documentation (if
    relevant)

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,4 +1,7 @@
 import { Metadata } from 'next';
+import { BulletedList, Heading1, Heading2 } from '@/components/ui/text-items';
+import { ArticlePage } from '@/components/ui/page';
+import { Section } from '@/components/ui/section';
 
 export const metadata: Metadata = {
   title: 'Privacy Policy – Civic Dashboard',
@@ -6,98 +9,115 @@ export const metadata: Metadata = {
 
 export default function Privacy() {
   return (
-    <div className="min-h-screen bg-white">
-      <main className="container mx-auto px-4 py-12 bg-white">
-        <div className="max-w-3xl mx-auto prose prose-lg">
-          <h1>Privacy Policy</h1>
+    <ArticlePage>
+      <Section>
+        <Heading1>Privacy Policy</Heading1>
+        <p>
+          We strive to keep this page up to date with a simple explanation of
+          what data about you we store or send to 3rd parties as part of
+          operating Civic Dashboard! This project is nascent, and we're aware we
+          need to flesh this out more.{' '}
+          <a href="/feedback" className="classic-link">
+            Let us know how we can do better!
+          </a>
+        </p>
+      </Section>
+      <Section>
+        <Heading2>Data you submit to us for our features to work</Heading2>
+        <p>
+          If you subscribe to a search on the actions page, we will store your
+          email address along with the associated search filters.
+        </p>
+      </Section>
+      <Section>
+        <Heading2>Data we collect to help us improve Civic Dashboard</Heading2>
+        <p>
+          We store analytics data alongside an anonimyzed time-bound identifier
+          with{' '}
+          <a href="https://umami.is/" target="_blank" className="classic-link">
+            Umami
+          </a>
+          . We make this data{' '}
+          <a
+            href="https://eu.umami.is/share/6R9CNotgCUNEmDL5/civicdashboard.ca"
+            target="_blank"
+            className="classic-link"
+          >
+            public
+          </a>{' '}
+          for transparency and accountability, and so that the value of this
+          data, which was created by the public, can be used by the public as
+          well.
+        </p>
 
-          <p>
-            We strive to keep this page up to date with a simple explanation of
-            what data about you we store or send to 3rd parties as part of
-            operating Civic Dashboard! This project is nascent, and we're aware
-            we need to flesh this out more.{' '}
-            <a href="/feedback">Let us know how we can do better! </a>
-          </p>
+        <p>
+          That means the pages you view and significant actions you take on the
+          website (but NOT any content you submit to the website) will be stored
+          alongside an identifier that can't be linked back to you, and which is
+          changed every month.
+        </p>
 
-          <h2>Data You Submit To Us For Our Features To Work</h2>
-          <p>
-            If you subscribe to a search on the actions page, we will store your
-            email address along with the associated search filters.
-          </p>
-
-          <h2>Data We Collect To Help Us Improve Civic Dashboard</h2>
-          <p>
-            We store analytics data alongside an anonimyzed time-bound
-            identifier with{' '}
-            <a href="https://umami.is/" target="_blank">
+        <p>
+          For example, we may store that you viewed our home page, then actions
+          page, then filtered by certain tags, then submitted a comment to
+          council, but NOT what that comment was.
+        </p>
+      </Section>
+      <Section>
+        <Heading2>Full list of 3rd party providers</Heading2>
+        <p>
+          Additionally, the 3rd party providers we use to host the website and
+          send emails have their own privacy policies. These providers are:
+        </p>
+        <BulletedList>
+          <li>
+            <a
+              href="https://www.cloudflare.com/en-ca/privacypolicy"
+              target="_blank"
+              className="classic-link"
+            >
+              Cloudflare
+            </a>
+          </li>
+          <li>
+            Sevalla, a service provided by{' '}
+            <a
+              href="https://kinsta.com/legal/privacy-policy/"
+              target="_blank"
+              className="classic-link"
+            >
+              Kinsta
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://resend.com/legal/privacy-policy"
+              target="_blank"
+              className="classic-link"
+            >
+              Resend
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://steadyhq.com/en/privacy"
+              target="_blank"
+              className="classic-link"
+            >
+              Steady
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://umami.is/privacy"
+              target="_blank"
+              className="classic-link"
+            >
               Umami
             </a>
-            . We make this data{' '}
-            <a
-              href="https://eu.umami.is/share/6R9CNotgCUNEmDL5/civicdashboard.ca"
-              target="_blank"
-            >
-              public
-            </a>{' '}
-            for transparency and accountability, and so that the value of this
-            data, which was created by the public, can be used by the public as
-            well.
-          </p>
-
-          <p>
-            That means the pages you view and significant actions you take on
-            the website (but NOT any content you submit to the website) will be
-            stored alongside an identifier that can't be linked back to you, and
-            which is changed every month.
-          </p>
-
-          <p>
-            For example, we may store that you viewed our home page, then
-            actions page, then filtered by certain tags, then submitted a
-            comment to council, but NOT what that comment was.
-          </p>
-
-          <h2>Full List of 3rd Party Providers</h2>
-          <p>
-            Additionally, the 3rd party providers we use to host the website and
-            send emails have their own privacy policies. These providers are:
-          </p>
-          <ul>
-            <li>
-              <a
-                href="https://www.cloudflare.com/en-ca/privacypolicy"
-                target="_blank"
-              >
-                Cloudflare
-              </a>
-            </li>
-            <li>
-              Sevalla, a service provided by{' '}
-              <a
-                href="https://kinsta.com/legal/privacy-policy/"
-                target="_blank"
-              >
-                Kinsta
-              </a>
-            </li>
-            <li>
-              <a href="https://resend.com/legal/privacy-policy" target="_blank">
-                Resend
-              </a>
-            </li>
-            <li>
-              <a href="https://steadyhq.com/en/privacy" target="_blank">
-                Steady
-              </a>
-            </li>
-            <li>
-              <a href="https://umami.is/privacy" target="_blank">
-                Umami
-              </a>
-            </li>
-          </ul>
-        </div>
-      </main>
-    </div>
+          </li>
+        </BulletedList>
+      </Section>
+    </ArticlePage>
   );
 }
