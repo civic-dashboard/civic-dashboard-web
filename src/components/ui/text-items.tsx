ad3cdce33a8bded154b1dbbe598172e772b5008c
antler-hat <dbusheikin@gmail.com>
2026-05-20 09:32:59 -0400

    Create new text component to handle text sizes with semantic control

--- src/components/ui/text-items.tsx
+++ src/components/ui/text-items.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import { type HTMLAttributes, type ReactNode } from 'react';
+import { cn } from '@/components/ui/utils';
 
-/**
- * Renders a `<h1>` with correct styling.
- */
-export function Heading1({ children }: { children: React.ReactNode }) {
-  return <h1 className="text-4xl font-bold mb-12">{children}</h1>;
-}
+type TextItemProps = {
+  children: ReactNode;
+  className?: string;
+};
 
-/**
- * Renders a `<h2>` with correct styling.
- */
-export function Heading2({ children }: { children: React.ReactNode }) {
-  return <h2 className="text-3xl font-semibold mb-6">{children}</h2>;
-}
+type TextPreset = 'Heading1' | 'Heading2' | 'Heading3' | 'Body' | 'Small';
+type TextElement = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
 
-/**
- * Renders a `<h3>` with correct styling.
- */
-export function Heading3({ children }: { children: React.ReactNode }) {
-  return <h3 className="text-xl font-bold mb-6">{children}</h3>;
+type TextProps = {
+  preset: TextPreset;
+  as?: TextElement; /* Optionally override the default HTML element for a11y compliance */
+  children: ReactNode;
+  className?: string;
+} & Omit<HTMLAttributes<HTMLElement>, 'children' | 'className'>;
+
+const textPresetElements: Record<TextPreset, TextElement> = {
+  Heading1: 'h1',
+  Heading2: 'h2',
+  Heading3: 'h3',
+  Body: 'p',
+  Small: 'p',
+};
+
+const textPresetClasses: Record<TextPreset, string> = {
+  Heading1:
+    'font-heading text-3xl md:text-4xl lg:text-5xl font-extrabold leading-[1.1] tracking-tight',
+  Heading2: 'font-heading text-2xl md:text-3xl font-bold leading-[1.2]',
+  Heading3: 'font-heading text-lg md:text-xl font-bold leading-[1.2]',
+  Body: 'font-body text-base font-normal leading-[1.5]',
+  Small: 'font-body text-sm font-normal leading-[1.5]',
+};
+
+export function Text({ preset, as, children, className, ...props }: TextProps) {
+  const Element = as ?? textPresetElements[preset];
+
+  return (
+    <Element className={cn(textPresetClasses[preset], className)} {...props}>
+      {children}
+    </Element>
+  );
 }
 
 /**
@@ -26,6 +48,10 @@ export function Heading3({ children }: { children: React.ReactNode }) {
  * elements. You do not need to add margin/padding for `<li>` -- this component
  * will provide it automatically.
  */
-export function BulletedList({ children }: { children: React.ReactNode }) {
-  return <ul className="list-disc mb-6 pl-6 space-y-4">{children}</ul>;
+export function BulletedList({ children, className }: TextItemProps) {
+  return (
+    <ul className={cn('text-base list-disc mb-6 pl-6 space-y-4', className)}>
+      {children}
+    </ul>
+  );
 }
