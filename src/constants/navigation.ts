74c34cf613b63d84e8dff1dc498a4107851318c4
imadsyed333 <creeperone23@gmail.com>
2026-07-23 23:59:52 -0400

    Created constants to help with link population

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,24 +1,61 @@
 export const menuItems = [
-  { label: 'Home', href: '/' },
+  { label: 'Council Activity', href: '/actions' },
+  {
+    label: 'Councillor Watch',
+    href: '/councillors',
+  },
   {
     label: 'How Council Works',
     href: '/how-council-works',
   },
-  { label: 'Actions', href: '/actions' },
   {
-    label: 'Councillors',
-    href: '/councillors',
-  },
-  {
-    label: 'About Us',
-    href: '/about',
-  },
-  {
-    label: 'Feedback',
-    href: '/feedback',
-  },
-  {
-    label: 'Wiki',
+    label: 'The Wiki',
     href: '/wiki',
+  },
+];
+
+export const resourceItems = [
+  { label: 'Source code', href: 'https://github.com/civic-dashboard' },
+  {
+    label: 'Slack',
+    href: 'https://civictechto.slack.com/archives/C06KU3DHEKV',
+  },
+  {
+    label: 'Documentation',
+    href: '',
+  },
+  {
+    label: 'Civic Tech Toronto',
+    href: 'https://civictech.ca/',
+  },
+];
+
+export const companyItems = [
+  { label: 'About us', href: '/about' },
+  { label: 'Contact', href: 'mailto:teamcivicdashboard@gmail.com' },
+  { label: 'Join us', href: '' },
+  { label: 'Sign up for our newsletter', href: '/join-newsletter' },
+];
+
+export const iconItems = [
+  {
+    icon: '/bluesky.svg',
+    href: 'https://bsky.app/profile/civicdashboard.bsky.social',
+    alt: 'Bluesky',
+  },
+  {
+    icon: '/linkedin.svg',
+    href: 'https://www.linkedin.com/company/civic-dashboard/about/',
+    alt: 'LinkedIn',
+  },
+  {
+    icon: '/github.svg',
+    href: 'https://github.com/civic-dashboard',
+    alt: 'GitHub',
+  },
+  {
+    icon: '/slack.svg',
+    href: 'https://civictechto.slack.com/archives/C06KU3DHEKV',
+    alt: 'Slack',
   },
 ];
