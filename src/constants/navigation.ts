d81754d0106b5e977d9bae534a772a65727d7461
imadsyed333 <creeperone23@gmail.com>
2026-07-25 20:33:03 -0400

    created new constants for footer links, added labels for umami analytics

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,24 +1,75 @@
 export const menuItems = [
-  { label: 'Council Activity', href: '/actions' },
-  {
-    label: 'Councillor Watch',
-    href: '/councillors',
-  },
+  { label: 'Home', href: '/' },
   {
     label: 'How Council Works',
     href: '/how-council-works',
   },
+  { label: 'Actions', href: '/actions' },
   {
-    label: 'The Wiki',
+    label: 'Councillors',
+    href: '/councillors',
+  },
+  {
+    label: 'About Us',
+    href: '/about',
+  },
+  {
+    label: 'Feedback',
+    href: '/feedback',
+  },
+  {
+    label: 'Wiki',
     href: '/wiki',
   },
 ];
 
-export const resourceItems = [
-  { label: 'Source code', href: 'https://github.com/civic-dashboard' },
+type FooterNavItem = {
+  label: string;
+  href: string;
+  umamiEvent?: string;
+};
+
+type FooterIconItem = {
+  icon: string;
+  href: string;
+  alt: string;
+  umamiEvent: string;
+};
+
+// Constants for the footer
+export const civicDashboardItems: FooterNavItem[] = [
+  {
+    label: 'Council Activity',
+    href: '/actions',
+    umamiEvent: 'Council Activity',
+  },
+  {
+    label: 'Councillor Watch',
+    href: '/councillors',
+    umamiEvent: 'Councillor Watch',
+  },
+  {
+    label: 'How Council Works',
+    href: '/how-council-works',
+    umamiEvent: 'How Council Works',
+  },
+  {
+    label: 'The Wiki',
+    href: '/wiki',
+    umamiEvent: 'The Wiki',
+  },
+];
+
+export const resourceItems: FooterNavItem[] = [
+  {
+    label: 'Source code',
+    href: 'https://github.com/civic-dashboard',
+    umamiEvent: 'GitHub',
+  },
   {
     label: 'Slack',
     href: 'https://civictechto.slack.com/archives/C06KU3DHEKV',
+    umamiEvent: 'Slack',
   },
   {
     label: 'Documentation',
@@ -27,35 +78,52 @@ export const resourceItems = [
   {
     label: 'Civic Tech Toronto',
     href: 'https://civictech.ca/',
+    umamiEvent: 'Civic Tech Toronto',
   },
 ];
 
-export const companyItems = [
-  { label: 'About us', href: '/about' },
-  { label: 'Contact', href: 'mailto:teamcivicdashboard@gmail.com' },
-  { label: 'Join us', href: '' },
-  { label: 'Sign up for our newsletter', href: '/join-newsletter' },
+export const companyItems: FooterNavItem[] = [
+  {
+    label: 'About us',
+    href: '/about',
+    umamiEvent: 'About us',
+  },
+  {
+    label: 'Contact',
+    href: 'mailto:teamcivicdashboard@gmail.com',
+    umamiEvent: 'Contact',
+  },
+  { label: 'Join us', href: '/join', umamiEvent: "Join us" },
+  {
+    label: 'Sign up for our newsletter',
+    href: '/join-newsletter',
+    umamiEvent: 'Newsletter Signup',
+  },
 ];
 
-export const iconItems = [
+export const iconItems: FooterIconItem[] = [
   {
     icon: '/bluesky.svg',
     href: 'https://bsky.app/profile/civicdashboard.bsky.social',
     alt: 'Bluesky',
+    umamiEvent: 'Bluesky',
   },
   {
     icon: '/linkedin.svg',
     href: 'https://www.linkedin.com/company/civic-dashboard/about/',
     alt: 'LinkedIn',
+    umamiEvent: 'LinkedIn',
   },
   {
     icon: '/github.svg',
     href: 'https://github.com/civic-dashboard',
     alt: 'GitHub',
+    umamiEvent: 'GitHub',
   },
   {
     icon: '/slack.svg',
     href: 'https://civictechto.slack.com/archives/C06KU3DHEKV',
     alt: 'Slack',
+    umamiEvent: 'Slack',
   },
 ];
