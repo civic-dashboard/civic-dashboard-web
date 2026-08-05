77b0ca0c48a77e73ee69bb6a9544535052945906
imadsyed333 <creeperone23@gmail.com>
2026-08-04 21:21:46 -0400

    Email preview displays current results of search, falls back to past results of search

--- src/backend/emails/subscriptions.tsx
+++ src/backend/emails/subscriptions.tsx
@@ -4,7 +4,10 @@ import { SearchOptions as SubscribableSearchFilters } from '@/logic/search';
 import * as subscriptions from '@/database/queries/subscriptions';
 import { createDB } from '@/database/kyselyDb';
 import { sendNewSubscriptionEmail } from '@/backend/emails/sendNewSubscriptionEmail';
-import { searchAgendaItems } from '@/database/queries/agendaItems';
+import {
+  AgendaItemSearchResult,
+  searchAgendaItems,
+} from '@/database/queries/agendaItems';
 import { allTags } from '@/constants/tags';
 import { decisionBodies } from '@/constants/decisionBodies';
 import { SubscriptionUpdateEmail } from '@/backend/emails/templates/subscriptionUpdate';
@@ -85,10 +88,47 @@ export async function previewSubscriptionEmail({
     tags,
     decisionBodyIds,
   };
+
+  const db = createDB();
+  const startOfToday = getStartOfToday();
+
+  let displayResults: AgendaItemSearchResult[] = [];
+
+  const currentResults = await searchAgendaItems(db, {
+    options: {
+      ...filters,
+      minimumDate: startOfToday,
+      sortBy: 'relevance',
+      sortDirection: 'descending',
+    },
+    pagination: {
+      page: 0,
+      pageSize: 20,
+    },
+  });
+
+  if (currentResults.totalCount > 0) {
+    displayResults = currentResults.results;
+  } else {
+    const pastResults = await searchAgendaItems(db, {
+      options: {
+        ...filters,
+        maximumDate: new Date(startOfToday.getTime() - 1),
+        sortBy: 'date',
+        sortDirection: 'descending',
+      },
+      pagination: {
+        page: 0,
+        pageSize: 10,
+      },
+    });
+    displayResults = pastResults.results;
+  }
+
   const previewHtml = await render(
     <SubscriptionUpdateEmail
       unsubscribeToken={PREVIEW_UNSUBSCRIBE_TOKEN}
-      items={SAMPLE_AGENDA_ITEMS}
+      items={displayResults}
       filters={[filters]}
     />,
   );
