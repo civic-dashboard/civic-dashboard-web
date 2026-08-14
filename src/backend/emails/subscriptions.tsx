bed3db3a471af64fb535c3e89743e545b68fa2dd
Imad Syed <64811136+imadsyed333@users.noreply.github.com>
2026-08-14 16:32:47 -0400

    feat-146: Subscription email preview (#474)
    ## Description
    
    <!-- 1. Link the related issue here. -->
    <!-- Replace "Resolves" with "Related to" if this PR should not close
    the issue (e.g. if the issue requires several PRs to complete) -->
    Resolves #146 
    
    <!-- 2. Give a high-level description of what this PR does. -->
    <!-- What was the behaviour before? What should it be now? -->
    Before, subscribing to agenda item search results would not provide
    users with an idea of what the email would look like. Now, users are
    shown a preview of the email current results (or if none, then past
    results) matching their search criteria.
    
    If the search criteria is too restrictive, users will be notified with a
    message under the email input.
    
    <!-- 3. If this PR results in something changing visually on the site,
    include before/after screenshots. -->
    <img width="1918" height="997" alt="image"
    src="https://github.com/user-attachments/assets/8f5da6eb-d8a9-4b9e-bbe9-3f03d095c1cd"
    />
    
    <img width="1918" height="1001" alt="image"
    src="https://github.com/user-attachments/assets/e9fbb3df-cd9a-4758-b063-6f8e638e8d56"
    />
    
    <!-- 4. If you have specific code review directions, include them here.
    -->
    <!-- E.g. "Look at these files first" or "Review one commit at a time"
    -->
    
    ## Testing instructions
    
    <!-- Describe how the reviewer should test this change. -->
    <!-- E.g. Which page should they look at? What should they click? What
    should happen after clicking? -->
    1. Enter a search query, a bunch of tags, and/or decision bodies
    2. Press the "Get Email Alerts" button. You should see an almost
    fullscreen modal appear with a place to input email and subscribe, as
    well as a preview of the email you will receive. If your search filters
    are too restrictive, you should also see a message notifying you of
    that.
    3. Pressing the "Subscribe" button should send you the email you were
    shown.
    
    ## Checklist
    
    <!-- Ensure you do these tasks before requesting a review! -->
    
    - [x] This PR addresses all requirements described in the issue
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have performed a self-review of my code
    - [x] I have tested these changes in my local environment
    - [x] I have tested these changes in the preview site generated by this
    PR (you must finish opening the PR first)
    - [x] I have made corresponding changes to the documentation (if
    relevant)

---
+++ src/backend/emails/subscriptions.tsx
@@ -0,0 +1,147 @@
+'use server';
+
+import { SearchOptions as SubscribableSearchFilters } from '@/logic/search';
+import * as subscriptions from '@/database/queries/subscriptions';
+import { createDB } from '@/database/kyselyDb';
+import { sendNewSubscriptionEmail } from '@/backend/emails/sendNewSubscriptionEmail';
+import {
+  AgendaItemSearchResult,
+  searchAgendaItems,
+} from '@/database/queries/agendaItems';
+import { allTags } from '@/constants/tags';
+import { decisionBodies } from '@/constants/decisionBodies';
+import { SubscriptionUpdateEmail } from '@/backend/emails/templates/subscriptionUpdate';
+import { render } from 'react-email';
+import { Kysely } from 'kysely';
+import { DB } from '@/database/allDbTypes';
+
+import { getStartOfToday } from '@/logic/date';
+
+const PREVIEW_UNSUBSCRIBE_TOKEN = 'preview';
+
+async function searchCurrentResults(
+  db: Kysely<DB>,
+  filters: SubscribableSearchFilters,
+) {
+  return searchAgendaItems(db, {
+    options: {
+      ...filters,
+      minimumDate: getStartOfToday(),
+      sortBy: 'relevance',
+      sortDirection: 'descending',
+    },
+    pagination: {
+      page: 0,
+      pageSize: 20,
+    },
+  });
+}
+
+type SubscribeToSearchArgs = {
+  email: string;
+  filters: SubscribableSearchFilters;
+};
+export async function subscribeToSearch({
+  email,
+  filters: { textQuery, tags, decisionBodyIds },
+}: SubscribeToSearchArgs) {
+  if (!email || !email.trim()) return;
+  const filters = {
+    textQuery,
+    tags,
+    decisionBodyIds,
+  };
+  const db = createDB();
+  const { unsubscribeToken } = await subscriptions.subscribeToSearch(db, {
+    email,
+    ...filters,
+  });
+
+  const currentResults = await searchCurrentResults(db, filters);
+  await sendNewSubscriptionEmail({
+    to: email,
+    props: {
+      unsubscribeToken,
+      items: currentResults.results,
+      filters: filters,
+    },
+  });
+
+  if (process.env.NEW_EMAIL_ALERT_WEBHOOK) {
+    await fetch(process.env.NEW_EMAIL_ALERT_WEBHOOK, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        tags: tags.map((t) => allTags[t].displayName).join(', '),
+        decisionBodies: decisionBodyIds
+          .map((id) => decisionBodies[id].decisionBodyName)
+          .join(', '),
+        textSearchUsed: textQuery ? 'Yes' : 'No',
+      }),
+    });
+  }
+}
+
+type PreviewSubscriptionEmailArgs = {
+  filters: SubscribableSearchFilters;
+};
+export async function previewSubscriptionEmail({
+  filters: { textQuery, tags, decisionBodyIds },
+}: PreviewSubscriptionEmailArgs) {
+  const filters = {
+    textQuery,
+    tags,
+    decisionBodyIds,
+  };
+
+  const db = createDB();
+  const startOfToday = getStartOfToday();
+
+  let displayResults: AgendaItemSearchResult[] = [];
+
+  const currentResults = await searchCurrentResults(db, filters);
+
+  if (currentResults.totalCount > 0) {
+    displayResults = currentResults.results;
+  } else {
+    const pastResults = await searchAgendaItems(db, {
+      options: {
+        ...filters,
+        maximumDate: new Date(startOfToday.getTime() - 1),
+        sortBy: 'date',
+        sortDirection: 'descending',
+      },
+      pagination: {
+        page: 0,
+        pageSize: 20,
+      },
+    });
+    displayResults = pastResults.results;
+  }
+
+  const previewHtml = await render(
+    <SubscriptionUpdateEmail
+      unsubscribeToken={PREVIEW_UNSUBSCRIBE_TOKEN}
+      items={displayResults}
+      filters={[filters]}
+    />,
+  );
+  return {
+    previewHtml,
+    hasMatchingResults: displayResults.length > 0,
+  };
+}
+
+type UnsubscribeFromSearchArgs = {
+  token: string;
+  id: number;
+};
+export async function unsubscribeFromSearch({
+  token,
+  id,
+}: UnsubscribeFromSearchArgs) {
+  const db = createDB();
+  await subscriptions.unsubscribeFromSearch(db, { token, subscriptionId: id });
+}
